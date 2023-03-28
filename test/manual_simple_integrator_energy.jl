6cf58c426f35be246e069b65cb1a9e1996cde820
gergaud <joseph.gergaud@toulouse-inp.fr>
2023-03-28 09:48:23 +0200

    unitary test on CTDirect_data

---
+++ test/manual_simple_integrator_energy.jl
@@ -0,0 +1,29 @@
+using CTDirect
+using CTBase
+using CTProblems
+
+
+# double integrator - energy min
+println("toto")
+prob = Problem(:integrator, :dim1, :energy)
+println("titi")
+ocp = prob.model
+
+# initial guess (constant state and control functions)
+#init = [1., 0.5, 0.3]
+
+# solve
+#sol = solve(ocp, grid_size=10, print_level=5)
+sol = solve(ocp, grid_size=100, print_level=5, tol=1e-12, mu_strategy="adaptive", init=nothing)
+
+p1 = plot(sol)
+
+
+#= # control box
+constraint!(ocp, :control, -4.01, 4.01, :control_con1)
+sol = solve(ocp, grid_size=100, print_level=5, tol=1e-12, mu_strategy="adaptive", init=init)
+
+# plot
+p2 =  plot(sol) =#
+
+plot(p1)
