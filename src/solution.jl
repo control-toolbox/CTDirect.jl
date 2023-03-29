3cf871086222a26ff24762dd5dbd3782e1b1c27a
Pierre Martinon <pierrecmartinon@gmail.com>
2023-03-29 16:25:20 +0200

    goddard example: added plot for state constraint and multiplier. Reworked internal parts of CTDirect (toplevel functions for NLP F and C, streamlined generation of OCP solution)

--- src/solution.jl
+++ src/solution.jl
@@ -1,9 +1,64 @@
-function parse_ipopt_sol(stats, ctd)
+# build generic OCP solution from direct method (NLP) solution
+# +++ put ocp inside ctd ?
+function _OptimalControlSolution(ocp, ipopt_solution, ctd)
+
+    # save general solution data
+    ctd.NLP_stats = ipopt_solution
+    ctd.NLP_objective = ipopt_solution.objective
+    ctd.NLP_constraints_violation = ipopt_solution.primal_feas
+    ctd.NLP_iterations = ipopt_solution.iter
+    ctd.NLP_solution = ipopt_solution.solution
+    ctd.NLP_sol_constraints = ipopt_constraint(ipopt_solution.solution, ctd)
+
+    # parse NLP variables, constraints and multipliers
+    X, U, P, CU, CX, CXU, mult_CU, mult_CX, mult_CXU = parse_ipopt_sol(ctd)
+
+    # interpolations to build functions of time
+    N = ctd.dim_NLP_steps
+    t0 = ctd.initial_time
+    tf = get_final_time(ctd.NLP_solution, ctd.final_time, ctd.has_free_final_time)
+    T = collect(LinRange(t0, tf, N+1))
+    x = ctinterpolate(T, matrix2vec(X, 1))
+    u = ctinterpolate(T, matrix2vec(U, 1))
+    p = ctinterpolate(T[1:end-1], matrix2vec(P, 1))
+    cx = ctinterpolate(T, matrix2vec(CX, 1))
+    mcx = ctinterpolate(T, matrix2vec(mult_CX, 1))
+
+    # build OptimalControlSolution struct
+    sol = OptimalControlSolution()
+    sol.state_dimension = ctd.state_dimension
+    sol.control_dimension = ctd.control_dimension
+    sol.times = T
+    sol.time_label = ocp.time_label
+    sol.state = t -> x(t)
+    sol.state_labels = ocp.state_labels
+    sol.adjoint = t -> p(t)
+    sol.control = t -> u(t)
+    sol.control_labels = ocp.control_labels
+    sol.objective = ctd.NLP_objective
+    sol.iterations = ctd.NLP_iterations
+    sol.stopping = :dummy 
+    sol.message = "no message" 
+    sol.success = false #
+    # field "infos" in OptimalControlSolution is Dict{Symbol, Any}, for misc data
+    # +++ todo: labels for costraints
+    sol.infos[:dim_state_constraints] = ctd.dim_state_constraints    
+    sol.infos[:state_constraints] = t -> cx(t)
+    sol.infos[:mult_state_constraints] = t -> mcx(t)
+
+
+    return sol
+
+end
+
+
+# parse NLP solution from ipopt into OCP variables, constraints and multipliers
+function parse_ipopt_sol(ctd)
     
     N = ctd.dim_NLP_steps
 
     # states and controls
-    xu = stats.solution
+    xu = ctd.NLP_solution
     X = zeros(N+1,ctd.dim_NLP_state)
     U = zeros(N+1,ctd.control_dimension)
     for i in 1:N+1
@@ -13,11 +68,14 @@ function parse_ipopt_sol(stats, ctd)
 
     # constraints, costate and constraints multipliers
     P = zeros(N, ctd.dim_NLP_state)
-    lambda = stats.multipliers
-    c = ctd.sol_constraints
-    P_control_constraints = zeros(N+1,ctd.dim_control_constraints)
-    P_state_constraints = zeros(N+1,ctd.dim_state_constraints)
-    P_mixed_constraints = zeros(N+1,ctd.dim_mixed_constraints)
+    lambda = ctd.NLP_stats.multipliers
+    c = ctd.NLP_sol_constraints
+    sol_control_constraints = zeros(N+1,ctd.dim_control_constraints)
+    sol_state_constraints = zeros(N+1,ctd.dim_state_constraints)
+    sol_mixed_constraints = zeros(N+1,ctd.dim_mixed_constraints)    
+    mult_control_constraints = zeros(N+1,ctd.dim_control_constraints)
+    mult_state_constraints = zeros(N+1,ctd.dim_state_constraints)
+    mult_mixed_constraints = zeros(N+1,ctd.dim_mixed_constraints)
     index = 1
     for i in 1:N
         # state equation
@@ -26,105 +84,36 @@ function parse_ipopt_sol(stats, ctd)
         # path constraints
         if ctd.has_control_constraints
             sol_control_constraints[i,:] = c[index:index+ctd.dim_control_constraints-1]
-            P_control_constraints[i,:] = lambda[index:index+ctd.dim_control_constraints-1]
+            mult_control_constraints[i,:] = lambda[index:index+ctd.dim_control_constraints-1]
             index = index + ctd.dim_control_constraints
         end
         if ctd.has_state_constraints
             sol_state_constraints[i,:] = c[index:index+ctd.dim_state_constraints-1]
-            P_state_constraints[i,:] = lambda[index:index+ctd.dim_state_constraints-1]
+            mult_state_constraints[i,:] = lambda[index:index+ctd.dim_state_constraints-1]
             index = index + ctd.dim_state_constraints
         end
         if ctd.has_mixed_constraints
             sol_mixed_constraints[i,:] = c[index:index+ctd.dim_mixed_constraints-1]
-            P_mixed_constraints[i,:] = lambda[index:index+ctd.dim_mixed_constraints-1]
+            mult_mixed_constraints[i,:] = lambda[index:index+ctd.dim_mixed_constraints-1]
             index = index + ctd.dim_mixed_constraints
         end
     end
     # path constraints at final time
     if ctd.has_control_constraints
         sol_control_constraints[N+1,:] = c[index:index+ctd.dim_control_constraints-1]
-        P_control_constraints[N+1,:] = lambda[index:index+ctd.dim_control_constraints-1]
+        mult_control_constraints[N+1,:] = lambda[index:index+ctd.dim_control_constraints-1]
         index = index + ctd.dim_control_constraints
     end
     if ctd.has_state_constraints
         sol_state_constraints[N+1,:] = c[index:index+ctd.dim_state_constraints-1] 
-        P_state_constraints[N+1,:] = lambda[index:index+ctd.dim_state_constraints-1]
+        mult_state_constraints[N+1,:] = lambda[index:index+ctd.dim_state_constraints-1]
         index = index + ctd.dim_state_constraints
     end
     if ctd.has_mixed_constraints
         sol_mixed_constraints[N+1,:] = c[index:index+ctd.dim_mixed_constraints-1]        
-        P_mixed_constraints[N+1,:] =  lambda[index:index+ctd.dim_mixed_constraints-1]
+        mult_mixed_constraints[N+1,:] =  lambda[index:index+ctd.dim_mixed_constraints-1]
         index = index + ctd.dim_mixed_constraints
     end
 
-    return X, U, P, sol_control_constraints, sol_state_constraints, sol_mixed_constraints, P_control_constraints, P_state_constraints, P_mixed_constraints
-end
-
-
-function DirectSolution(ocp, N, ipopt_solution, init, constraints)
-
-    ctd = CTDirect_data(ocp, N, init)
-    ctd.sol_constraints = constraints
-
-    # state, control, adjoint
-    X, U, P, sol_control_constraints, sol_state_constraints, sol_mixed_constraints, P_control_constraints, P_state_constraints, P_mixed_constraints = parse_ipopt_sol(ipopt_solution, ctd)
-    
-    # times
-    t0 = ctd.initial_time
-    tf = get_final_time(ipopt_solution.solution, ctd.final_time, ctd.has_free_final_time)
-    T = collect(LinRange(t0, tf, N+1))
-    
-    # misc info
-    objective = ipopt_solution.objective
-    constraints_violation = ipopt_solution.primal_feas
-    iterations = ipopt_solution.iter
-    #status = ipopt_solution.status this is a 'Symbol' not an int... 
-
-    # save solution data
-    ctd.T = T 
-    ctd.X = X
-    ctd.U = U 
-    ctd.P = P
-    ctd.sol_control_constraints = sol_control_constraints
-    ctd.sol_state_constraints = sol_state_constraints
-    ctd.sol_mixed_constraints = sol_mixed_constraints    
-    ctd.P_control_constraints = P_control_constraints
-    ctd.P_state_constraints = P_state_constraints
-    ctd.P_mixed_constraints = P_mixed_constraints
-    ctd.objective = objective
-    ctd.constraints_violation = constraints_violation
-    ctd.iterations = iterations
-    ctd.stats = ipopt_solution
-
-    return _OptimalControlSolution(ocp, ctd)
-end
-
-
-function _OptimalControlSolution(ocp, ctd)
-
-    # je ne peux pas donner directement la sortie de ctinterpolate car ce n'est pas une Function. CTBase doit etre mis a jour (?)
-    # matrix2vec is in CTBase/src/utils.jl
-    x = ctinterpolate(ctd.T, matrix2vec(ctd.X, 1))
-    u = ctinterpolate(ctd.T, matrix2vec(ctd.U, 1))
-    p = ctinterpolate(ctd.T[1:end-1], matrix2vec(ctd.P, 1)) 
-    sol = OptimalControlSolution()
-    sol.state_dimension = ctd.state_dimension
-    sol.control_dimension = ctd.control_dimension
-    sol.times = ctd.T
-    sol.time_label = ocp.time_label
-    sol.state = t -> x(t)
-    sol.state_labels = ocp.state_labels # update CTBase to have a getter ?
-    sol.adjoint = t -> p(t)
-    sol.control = t -> u(t)
-    sol.control_labels = ocp.control_labels
-    sol.objective = ctd.objective
-    sol.iterations = ctd.iterations
-    # sync with CTDirectShooting: :optimality, :stagnation, :iterations
-    sol.stopping = :dummy 
-    # see CTDirectShooting/src/solve.jl : textsStopping
-    sol.message = "no message" 
-    sol.success = false #
-    # field "infos" in OptimalControlSolution is Dict{Symbol, Any}, for misc data
-    # +++ constraints and multipliers 
-    return sol
+    return X, U, P, sol_control_constraints, sol_state_constraints, sol_mixed_constraints, mult_control_constraints, mult_state_constraints, mult_mixed_constraints
 end
--- test/manual_test_goddard.jl
+++ test/manual_test_goddard.jl
@@ -1,7 +1,7 @@
 using CTDirect
 using CTProblems
 using CTBase # for plot
-using ADNLPModels
+using Plots;
 
 # goddard with state constraint - maximize altitude
 prob = Problem(:goddard, :state_constraint)
@@ -10,8 +10,21 @@ ocp = prob.model
 # initial guess (constant state and control functions)
 init = [1.01, 0.05, 0.8, 0.1]
 
-#sol = solve(ocp, grid_size=20, print_level=5)
+# solve problem
 sol = solve(ocp, grid_size=100, print_level=5, tol=1e-12, mu_strategy="adaptive", init=init)
 
 # plot
 plot(sol)
+
+# additional plots for constraint and multipliers (use info field from sol)
+# +++todo: retrieve individual labels for constraints
+t0 = sol.times[1]
+tf = last(sol.times)
+ncx = sol.infos[:dim_state_constraints]
+PCX = Array{Plots.Plot, 1}(undef, ncx);
+for i in 1:ncx
+    PCX[i] = plot(t -> sol.infos[:state_constraints](t)[i], t0, tf, label="constraint", legend=:topleft)
+    PCX[i] = plot!(twinx(), t -> sol.infos[:mult_state_constraints](t)[i], t0, tf, color=:red, label="multiplier", xticks=:none) #2nd scale
+    #PCX[i] = plot!(t -> sol.infos[:mult_state_constraints](t)[i], t0, tf, color=:red, label="multiplier")
+end
+plot(PCX..., layout = (ncx,1));
