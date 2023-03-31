bf9f711a1ac4e153e3298e7173cc3203dd9e4946
Pierre Martinon <pierrecmartinon@gmail.com>
2023-03-31 16:50:07 +0200

    version 0.1.4 (#20)
    * version 0.1.4
    
    * use goddard with all constraint types in test

--- test/test_goddard.jl
+++ test/test_goddard.jl
@@ -6,6 +6,7 @@ ocp = prob.model
 # initial guess (constant state and control functions)
 init = [1.01, 0.05, 0.8, 0.1]
 
+#=
 # state constraint formulation
 println("State constraint formulation")
 #println(constraints(ocp))
@@ -13,6 +14,7 @@ sol = solve(ocp, grid_size=10, print_level=0, init=init)
 @testset verbose = true showtiming = true ":goddard :state_constraint" begin
     @test sol.objective ≈ prob.solution.objective atol=5e-3
 end
+=#
 
 #= # mixed constraint formulation
 println("Mixed constraint formulation")
@@ -23,6 +25,7 @@ sol = solve(ocp, grid_size=10, print_level=0, init=init)
     @test sol.objective ≈ prob.solution.objective atol=5e-3
 end =#
 
+#=
 # box constraint formulation
 println("Box constraint formulation")
 #println(constraints(ocp))
@@ -35,5 +38,26 @@ constraint!(ocp, :control, Index(1), 0, 1, :box_control)
 sol = solve(ocp, grid_size=10, print_level=0, init=init)
 println(sol.objective)
 @testset verbose = true showtiming = true ":goddard :box_constraint" begin
+    @test sol.objective ≈ prob.solution.objective atol=5e-3
+end
+=#
+
+# all constraints formulation
+remove_constraint!(ocp,:state_constraint_r)
+remove_constraint!(ocp,:state_constraint_v)
+remove_constraint!(ocp,:control_constraint)
+# state constraint
+constraint!(ocp, :state, x->x[2], -Inf, 0.1, :state_con_vmax)
+# control constraint
+constraint!(ocp, :control, u->u, -Inf, 1, :control_con_umax)
+# mixed constraint
+constraint!(ocp, :mixed, (x,u)->x[3], 0.6, Inf, :mixed_con_mmin)
+# state box
+constraint!(ocp, :state, Index(1), 1, Inf, :state_box_rmin)
+constraint!(ocp, :state, Index(2), 0, Inf, :state_box_vmin)
+# control box
+constraint!(ocp, :control, Index(1), 0, Inf, :control_box_umin)
+sol = solve(ocp, grid_size=10, print_level=0, init=init)
+@testset verbose = true showtiming = true ":goddard :all_constraints" begin
     @test sol.objective ≈ prob.solution.objective atol=5e-3
 end
