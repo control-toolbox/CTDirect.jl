8c9876798af087b02620ac7d1b6c158f54160055
PierreMartinon <pierrecmartinon@gmail.com>
2023-04-05 19:52:45 +0200

    check exponential conso min

--- test/test_goddard.jl
+++ test/test_goddard.jl
@@ -1,32 +1,14 @@
-# goddard with state constraint - maximize altitude  +++ use newer one in CTProblems
+# goddard with state constraint - maximize altitude
 println("Goddard test")
-prob = Problem(:goddard, :state_constraint)
-ocp = prob.model
-
-# initial guess (constant state and control functions)
+prob = Problem(:goddard, :all_constraint)
 init = [1.01, 0.05, 0.8, 0.1]
-
-# all constraints formulation
-remove_constraint!(ocp,:state_constraint_r)
-remove_constraint!(ocp,:state_constraint_v)
-remove_constraint!(ocp,:control_constraint)
-# state constraint
-constraint!(ocp, :state, x->x[2], -Inf, 0.1, :state_con_vmax)
-# control constraint
-constraint!(ocp, :control, u->u, -Inf, 1, :control_con_umax)
-# mixed constraint
-constraint!(ocp, :mixed, (x,u)->x[3], 0.6, Inf, :mixed_con_mmin)
-# state box
-constraint!(ocp, :state, Index(1), 1, Inf, :state_box_rmin)
-constraint!(ocp, :state, Index(2), 0, Inf, :state_box_vmin)
-# control box
-constraint!(ocp, :control, Index(1), 0, Inf, :control_box_umin)
-sol = solve(ocp, grid_size=10, print_level=0, init=init)
+sol = solve(prob.model, grid_size=10, print_level=0, init=init)
 @testset verbose = true showtiming = true ":goddard :all_constraints" begin
-    @test sol.objective ≈ prob.solution.objective atol=5e-3
+    @test sol.objective ≈ prob.solution.objective rtol=1e-2
 end
 
 # +++ add automatic tests for multipliers, ie sign and complementarity with constraints ?
+#=
 t = sol.times
 # state constraint v <= 0.1
 a = sol.infos[:mult_state_constraints](t)[1]
@@ -36,6 +18,7 @@ b = sol.infos[:state_constraints](t)[1]
 println(b)
 println(a.*b)
 println(norm(a.*b))
+=#
 
 # control constraint u <= 1
 
