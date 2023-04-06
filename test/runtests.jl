178e638a9a75adf51f174085f3653a21219a9876
PierreMartinon <pierrecmartinon@gmail.com>
2023-04-06 12:38:48 +0200

    no glyphs in code please

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,31 +6,15 @@ using LinearAlgebra
 
 include("test_utils.jl")
 
-#=
-# test of optimal control problem from CTProblems
-@testset verbose = true showtiming = true "Direct" begin
-    for name in (
-        "integrator",
-        "goddard",
-        )
-        @testset "$name" begin
-            include("test_$name.jl")
-        end
-    end
-end
-=#
-
-#generate_coverage(; run_test = true) ??
-
 # test all problems in CTProblems
 @testset verbose = true showtiming = true "All problems" begin
     problems_list = Problems()
     for problem_description in problems_list
-        if :consumption ∈ problem_description
-            nothing # smooth it?
+        if :consumption in problem_description
+            println("Skip: ", prob.description)
         else
             prob = Problem(problem_description)
-            println(prob.description)
+            println("Test: ",prob.description)
             @testset "$(prob.description)" begin
                 sol = solve(prob.model, grid_size=20, print_level=0)
                 @test sol.objective ≈ prob.solution.objective rtol=1e-2
