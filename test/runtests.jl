1e251bf313257316ef0f3aa6029e184178d85df6
PierreMartinon <pierrecmartinon@gmail.com>
2023-04-06 12:59:41 +0200

    fixed tests. ready to merge and release 0.2.2 hopefully

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,10 +11,10 @@ include("test_utils.jl")
     problems_list = Problems()
     for problem_description in problems_list
         if :consumption in problem_description
-            println("Skip: ", prob.description)
+            println("Skip: ", problem_description)
         else
             prob = Problem(problem_description)
-            println("Test: ",prob.description)
+            println("Test: ",problem_description)
             @testset "$(prob.description)" begin
                 sol = solve(prob.model, grid_size=20, print_level=0)
                 @test sol.objective ≈ prob.solution.objective rtol=1e-2
