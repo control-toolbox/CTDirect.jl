be7120edf67dc0acf36e1a70153222c71c62799b
PierreMartinon <pierrecmartinon@gmail.com>
2023-06-01 13:43:52 +0200

    more double integrator tests

--- test/suite/double_integrator.jl
+++ test/suite/double_integrator.jl
@@ -1,6 +1,7 @@
 using CTDirect
 using CTBase
 
+println("Test: double integrator")
 
 # min tf
 ocp1 = Model(variable=true)
@@ -13,9 +14,7 @@ constraint!(ocp1, :final, [1,0], :final_constraint)
 constraint!(ocp1, :control, -1, 1, :control_constraint)
 constraint!(ocp1, :variable, 0.1, 10, :variable_constraint)
 dynamics!(ocp1, (x, u, v) ->  [x[2], u])
-objective!(ocp1, :mayer, (x0, xf, v) -> v) # scalar variable
-#objective!(ocp3, :mayer, (x0, xf, v) -> - v[1]) # max t0 free to and tf ok
-#objective!(ocp3, :mayer, (x0, xf, v) -> v[1], :max) # max t0 free to and tf ok
+objective!(ocp1, :mayer, (x0, xf, v) -> v)
 sol1 = solve(ocp1, grid_size=100, print_level=0, tol=1e-12)
 @testset verbose = true showtiming = true ":double_integrator :min_tf" begin
     @test sol1.objective ≈ 2.0 rtol=1e-2
@@ -73,4 +72,39 @@ objective!(ocp4, :mayer, (x0, xf, v) -> v[1], :max)
 sol4 = solve(ocp4, grid_size=100, print_level=0, tol=1e-12)
 @testset verbose = true showtiming = true ":double_integrator :max_t0" begin
     @test sol4.objective ≈ 8.0 rtol=1e-2
+end
+
+
+# min tf, abstract definition
+@def ocp begin
+    tf ∈ R, variable
+    t ∈ [ 0, tf ], time
+    x ∈ R², state
+    u ∈ R, control
+    -1 ≤ u(t) ≤ 1
+    x(0) == [ 0, 0 ]
+    x(tf) == [ 1, 0 ]
+    0.1 ≤ tf ≤ Inf 
+    ẋ(t) == [ x₂(t), u(t) ] 
+    tf → min
+end
+sol = solve(ocp, grid_size=100, print_level=0, tol=1e-12)
+@testset verbose = true showtiming = true ":double_integrator :min_tf :abstract" begin
+    @test sol.objective ≈ 2.0 rtol=1e-2
+end
+
+
+# min energy dual control
+ocp5 = Model()
+state!(ocp5, 2)
+control!(ocp5, 2)
+time!(ocp5, 0, 5)
+constraint!(ocp5, :initial, [0,0], :initial_constraint)
+constraint!(ocp5, :final, [1,0], :final_constraint)
+constraint!(ocp5, :control, 1:2, [0,0], [1,1], :control_box) # [u_, u+]
+dynamics!(ocp5, (x, u) ->  [x[2], -u[1] + u[2]])
+objective!(ocp5, :lagrange, (x, u) -> u[1]*u[1] + u[2]*u[2])
+sol5 = solve(ocp5, grid_size=100, print_level=0, tol=1e-12)
+@testset verbose = true showtiming = true ":double_integrator :min_energy" begin
+    @test sol5.objective ≈ 9.6e-2 rtol=1e-2
 end
---
+++ test/suite/goddard_all_constraints.jl
@@ -0,0 +1,58 @@
+using CTDirect
+using CTBase
+
+println("Test: goddard all constraints")
+# goddard max final altitue (all constraint types formulation)
+ocp = Model(variable=true)
+Cd = 310
+Tmax = 3.5
+β = 500
+b = 2
+r0 = 1
+v0 = 0
+vmax = 0.1
+m0 = 1
+mf = 0.6
+x0 = [ r0, v0, m0 ]
+state!(ocp, 3)
+control!(ocp, 1)
+variable!(ocp, 1)
+time!(ocp, 0, Index(1))
+# use all possible types of constraints
+# initial condition
+constraint!(ocp, :initial, x0, :initial_constraint)
+# final condition
+constraint!(ocp, :final, Index(3), mf, :final_constraint)
+# state constraint
+constraint!(ocp, :state, x->x[2], -Inf, vmax, :state_con_v_ub)
+# control constraint
+constraint!(ocp, :control, u->u, -Inf, 1, :control_con_u_ub)
+# mixed constraint
+constraint!(ocp, :mixed, (x,u)->x[3], mf, Inf, :mixed_con_m_lb)
+# variable constraint
+constraint!(ocp, :variable, v->v, -Inf, 10, :variable_con_tf_ubx)
+# state box
+constraint!(ocp, :state, 1:2, [r0,v0], [r0+0.2, Inf], :state_box_rv)
+#constraint!(ocp, :state, Index(2), v0, Inf, :state_box_vmin)
+# control box
+constraint!(ocp, :control, Index(1), 0, Inf, :control_box_lb)
+# variable box
+constraint!(ocp, :variable, Index(1), 0.01, Inf, :variable_box_tfmin)
+objective!(ocp, :mayer,  (x0, xf, v) -> xf[1], :max)
+function F0(x)
+    r, v, m = x
+    D = Cd * v^2 * exp(-β*(r - 1))
+    return [ v, -D/m - 1/r^2, 0 ]
+end
+function F1(x)
+    r, v, m = x
+    return [ 0, Tmax/m, -b*Tmax ]
+end
+dynamics!(ocp, (x, u, v) -> F0(x) + u*F1(x) )
+
+init_constant = [1.05, 0.1, 0.8, 0.5, 0.1]
+sol = solve(ocp, grid_size=100, print_level=0, tol=1e-12, init=init_constant)
+println(sol.objective)
+#@testset verbose = true showtiming = true ":goddard :max_rf :all_constraints_types" begin
+#    @test sol.objective ≈  rtol=1e-2
+#end
