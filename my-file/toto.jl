136c60620d2a108e8c5fd046fc26c0802a01a0cc
gergaud <joseph.gergaud@toulouse-inp.fr>
2023-06-30 14:20:30 +0200

    type

--- my-file/toto.jl
+++ my-file/toto.jl
@@ -1,9 +1,33 @@
-function main(xu)
-a::eltype(xu) = 1.
+using Revise
+function main(xu::Vector{<:Real})
+println("xu = ", xu)
+a::eltype(xu) = 1
+println("typeof(a) = ", typeof(a))
 return a
 end
 
-b = main([4.,7])
-
+c = [4,7]
+println("b = main(c)")
+println("typeof(c) = ", typeof(c))
+b = main(c)
 println("typeof(b) = ", typeof(b))
+
+c = [4.,7]
+println("b = main(c)")
+println("typeof(c) = ", typeof(c))
+b = main(c)
+println("typeof(b) = ", typeof(b))
+
+#= include("my-file/toto.jl")
+b = main(c)
+typeof(c) = Vector{Int64}
+xu = [4, 7]
+typeof(a) = Int64
+typeof(b) = Int64
+b = main(c)
+typeof(c) = Vector{Float64}
+xu = [4.0, 7.0]
+typeof(a) = Float64
+typeof(b) = Float64
+=#
 
