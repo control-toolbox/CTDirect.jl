d00bf28a9ad2aaaf0781d162eb65b8ef2e57e5b1
Pierre Martinon <pierrecmartinon@gmail.com>
2024-03-18 13:59:09 +0100

    use a const copy of ocp in docp

--- test/test_export_docp.jl
+++ test/test_export_docp.jl
@@ -1,8 +1,7 @@
 using CTDirect
 using CTBase
 
-println("Test simple integrator: old formulation")
-# min energy
+# simple integrator min energy
 ocp1 = Model()
 state!(ocp1, 1)
 control!(ocp1, 1)
@@ -11,11 +10,10 @@ constraint!(ocp1, :initial, -1, :initial_constraint)
 constraint!(ocp1, :final, 0, :final_constraint)
 dynamics!(ocp1, (x, u) -> -x + u)
 objective!(ocp1, :lagrange, (x, u) -> u^2)
-sol1 = solve(ocp1, grid_size=100, print_level=0, tol=1e-12)
-println("Expected Objective 0.313, found ", sol1.objective)
 
 println("Test simple integrator: new formulation with export")
 docp = DirectTranscription(ocp1, grid_size=100)
 sol2 = solveDOCP(docp, print_level=0, tol=1e-12)
 println("Expected Objective 0.313, found ", sol2.objective)
 
+#@code_warntype 
