15c12e8acd14c9ee0e0f9dabeca6cc5f4f11c11f
Pierre Martinon <pierrecmartinon@gmail.com>
2024-03-28 11:18:43 +0100

    todo: recheck test suite with vector getters only

--- test/test_scalar_vector.jl
+++ test/test_scalar_vector.jl
@@ -1,12 +1,9 @@
 # try to determine the efficiency of the scalar/vector handling
 
-dimx = 3
-dimu = 1
 N = 10
-xu = ones((N+1)*dimx+N*dimu)
+xu = ones((N+1)*n+N*m)
 
-function getx(xu, step)
-    n = dimx
+function getx(xu, step, n)
     if n == 1
         return xu[step*n+1]
     else
@@ -14,37 +11,33 @@ function getx(xu, step)
     end
 end
 
-function getu(xu, step)
-    n = dimx
-    m = dimu
-    if m == 1
-        return xu[(N+1)*n + step*m + 1]
-    else
+function getu(xu, step, n, m)
+    #if m == 1
+    #    return xu[(N+1)*n + step*m + 1]
+    #else
         return xu[(N+1)*n + step*m + 1 : (N+1)*n + step*m + m]
-    end
+    #end
+end
 
-function dynamics_scalar(x, u)
-    f = zeros(dimx)
+function dynamics_scalar(x, u, n)
+    f = zeros(n)
     f[1] = x[2]
     f[2] = u
     f[3] = u*u
     return f
 end
 
-function dynamics_vector(x, u)
-    f = zeros(dimx)
+function dynamics_vector(x, u, n)
+    f = zeros(n)
     f[1] = x[2]
     f[2] = u[1]
     f[3] = u[1]*u[1]
     return f
 end
 
-#compile
-step = 5
-dynamics_scalar(getx(xu,step), getu(xu,step))
-dynamics_vector(getx(xu,step), getu(xu,step))
+# ici u semble bien de type scalaire des la premiere evaluation
+@code_warntype dynamics_scalar(getx(xu,5,3), getu(xu,5,3,1))
+@code_warntype dynamics_vector(getx(xu,5,3), getu(xu,5,3,1))
 
-#dynamic dispatch ?
-step = 6
-@code_warntype dynamics_scalar(getx(xu,step), getu(xu,step)) 
-@code_warntype dynamics_vector(getx(xu,step), getu(xu,step)) 
+# nb le code dynamics_scalar passe aussi avec un u vectoriel
+# au niveau de CTBase qu'est ce qui coince exactement si on attend un argument scalaire et qu'on recupere un vecteur de taille 1 ?
