8e86d64a427dcef888d56921ec2653ee582507fa
Pierre Martinon <pierrecmartinon@gmail.com>
2024-04-12 11:30:45 +0200

    added failsafe in solution generation to ensure that t0 > tf (#76)
    * added failsafe in solution generation to ensure that t0 > tf
    
    * some reorg in test scripts

--- test/suite/all_constraints_types.jl
+++ test/suite/all_constraints_types.jl
@@ -1,6 +1,6 @@
 using CTDirect
 
-println("Test: goddard all constraints")
+println("Test: all constraint types")
 
 # goddard max final altitue (all constraint types formulation)
 ocp = Model(variable=true)
@@ -50,7 +50,7 @@ function F1(x)
 end
 dynamics!(ocp, (x, u, v) -> F0(x) + u*F1(x) )
 
-@testset verbose = true showtiming = true ":goddard :max_rf :all_constraints_types" begin
+@testset verbose = true showtiming = true ":goddard :max_rf :all_constraints" begin
     sol1 = solveDirect(ocp, grid_size=100, print_level=0, tol=1e-8)
     @test sol1.objective ≈ 1.0125 rtol=1e-2
 end
@@ -60,64 +60,64 @@ x_init = [1.05, 0.1, 0.8]
 u_init = 0.5
 v_init = 0.1
 
-@testset verbose = true showtiming = true ":goddard :max_rf :all_constraints_types :init_constant" begin
+@testset verbose = true showtiming = true ":goddard :max_rf :all_constraints :init_constant" begin
     init_constant = OptimalControlInit(x_init=x_init, u_init=u_init, v_init=v_init)
     sol2 = solveDirect(ocp, grid_size=30, print_level=0, tol=1e-8, init=init_constant)
     @test sol2.objective ≈ 1.0125 rtol=1e-2
 end
 
-@testset verbose = true showtiming = true ":goddard :max_rf :all_constraints_types :init_function_x" begin
+@testset verbose = true showtiming = true ":goddard :max_rf :all_constraints :init_function_x" begin
     init_function_x = OptimalControlInit(x_init=t->x_init, u_init=u_init, v_init=v_init)
     sol = solveDirect(ocp, grid_size=30, print_level=0, tol=1e-8, init=init_function_x)
     @test sol.objective ≈ 1.0125 rtol=1e-2
 end
 
-@testset verbose = true showtiming = true ":goddard :max_rf :all_constraints_types :init_function_u" begin
+@testset verbose = true showtiming = true ":goddard :max_rf :all_constraints :init_function_u" begin
     init_function_u = OptimalControlInit(x_init=x_init, u_init=t->u_init, v_init=v_init)
     sol = solveDirect(ocp, grid_size=30, print_level=0, tol=1e-8, init=init_function_u)
     @test sol.objective ≈ 1.0125 rtol=1e-2
 end
 
-@testset verbose = true showtiming = true ":goddard :max_rf :all_constraints_types :init_function_xu" begin
+@testset verbose = true showtiming = true ":goddard :max_rf :all_constraints :init_function_xu" begin
     init_function_xu = OptimalControlInit(x_init=t->x_init, u_init=t->u_init, v_init=v_init)
     sol = solveDirect(ocp, grid_size=30, print_level=0, tol=1e-8, init=init_function_xu)
     @test sol.objective ≈ 1.0125 rtol=1e-2
 end
 
-@testset verbose = true showtiming = true ":goddard :max_rf :all_constraints_types :init_constant (x)" begin
+@testset verbose = true showtiming = true ":goddard :max_rf :all_constraints :init_constant (x)" begin
     sol3 = solveDirect(ocp, grid_size=30, print_level=0, tol=1e-8, init=OptimalControlInit(x_init=x_init))
     @test sol3.objective ≈ 1.0125 rtol=1e-2
 end
 
-@testset verbose = true showtiming = true ":goddard :max_rf :all_constraints_types :init_constant (u)" begin
+@testset verbose = true showtiming = true ":goddard :max_rf :all_constraints :init_constant (u)" begin
     sol4 = solveDirect(ocp, grid_size=30, print_level=0, tol=1e-8, init=OptimalControlInit(u_init=u_init))
     @test sol4.objective ≈ 1.0125 rtol=1e-2
 end
 
-@testset verbose = true showtiming = true ":goddard :max_rf :all_constraints_types :init_constant (v)" begin
+@testset verbose = true showtiming = true ":goddard :max_rf :all_constraints :init_constant (v)" begin
     sol5 = solveDirect(ocp, grid_size=30, print_level=0, tol=1e-8, init=OptimalControlInit(v_init=v_init))
     @test sol5.objective ≈ 1.0125 rtol=1e-2
 end
 
-@testset verbose = true showtiming = true ":goddard :max_rf :all_constraints_types :init_constant (x,u)" begin
+@testset verbose = true showtiming = true ":goddard :max_rf :all_constraints :init_constant (x,u)" begin
     sol6 = solveDirect(ocp, grid_size=30, print_level=0, tol=1e-8, init=OptimalControlInit(x_init=x_init, u_init=u_init))
     @test sol6.objective ≈ 1.0125 rtol=1e-2
 end
 
-@testset verbose = true showtiming = true ":goddard :max_rf :all_constraints_types :init_constant (x,v)" begin
+@testset verbose = true showtiming = true ":goddard :max_rf :all_constraints :init_constant (x,v)" begin
     sol7 = solveDirect(ocp, grid_size=30, print_level=0, tol=1e-8, init=OptimalControlInit(x_init=x_init, v_init=v_init))
     @test sol7.objective ≈ 1.0125 rtol=1e-2
 end
 
 
-@testset verbose = true showtiming = true ":goddard :max_rf :all_constraints_types :init_constant (u,v)" begin
+@testset verbose = true showtiming = true ":goddard :max_rf :all_constraints :init_constant (u,v)" begin
     sol8 = solveDirect(ocp, grid_size=30, print_level=0, tol=1e-8, init=OptimalControlInit(u_init=u_init, v_init=v_init))
     @test sol8.objective ≈ 1.0125 rtol=1e-2
 end
 
 # with initial guess from solution
 sol = solveDirect(ocp, grid_size=100, print_level=0, tol=1e-8)
-@testset verbose = true showtiming = true ":goddard :max_rf :all_constraints_types :init_sol" begin
+@testset verbose = true showtiming = true ":goddard :max_rf :all_constraints :init_sol" begin
     init_sol = OptimalControlInit(sol)
     sol9 = solveDirect(ocp, grid_size=30, print_level=0, tol=1e-8, init=init_sol)
     @test sol9.objective ≈ 1.0125 rtol=1e-2
