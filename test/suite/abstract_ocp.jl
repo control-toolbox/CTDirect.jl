d0e57a925b6515b20b30e4caa2db5984c00219ed
Pierre Martinon <pierrecmartinon@gmail.com>
2024-04-18 12:14:25 +0200

    abstarct goddardok, added to CI test

--- test/suite/abstract_ocp.jl
+++ test/suite/abstract_ocp.jl
@@ -47,32 +47,40 @@ end
     @test sol2.objective ≈ 5.46 rtol=1e-2
 end
 
-
+Cd = 310
+β = 500
+Tmax = 3.5
+b = 2
+vmax = 0.1
+function F0(x)
+    r, v, m = x
+    D = Cd * v^2 * exp(-β*(r - 1))
+    return [ v, -D/m - 1/r^2, 0 ]
+end
+function F1(x)
+    r, v, m = x
+    return [ 0, Tmax/m, -b*Tmax ]
+end
+# NB. the ≤ is not the same as <= (parse error for <=)
 @def ocp3 begin
     tf ∈ R, variable
     t ∈ [ 0, tf ], time
-    0.1 ≤ tf ≤ 10
     x ∈ R^3, state
-    r = x_1
-    v = x_2
-    m = x_3
+    u ∈ R, control
+    0.1 ≤ tf ≤ 10
+    r = x[1]
+    v = x[2]
+    m = x[3]
     x(0) == [1, 0, 1]
     m(tf) == 0.6
-    v(t) <= 0.1
-    u ∈ R, control
+    1 ≤ r(t) ≤ 1.1
+    0 ≤ v(t) ≤ vmax
     0 ≤ u(t) ≤ 1
-    Cd = 310
-    β = 500
-    Tmax = 3.5
-    b = 2
-    D = Cd * v^2 * exp(-β*(r - 1))
-    F0: x -> [v, -D/m - 1/r^2, 0]
-    F1: x -> [ 0, Tmax/m, -b*Tmax ]
-    ẋ(t) == F0(x(t)) + u(t)*F1(x(t)) 
-    tf → min
+    ẋ(t) == F0(x(t)) + u(t)*F1(x(t))
+    r(tf) → max
 end
 
-@testset verbose = true showtiming = true ":goddard :max_rf :abstract" begin
-    sol3 = solve(ocp3, grid_size=100, print_level=0, tol=1e-12)
-    @test sol2.objective ≈ 1.0125 rtol=1e-2
+@testset verbose = true showtiming = true ":double_integrator :min_tf :abstract :constr" begin
+    sol3 = solve(ocp3, grid_size=100, print_level=0, tol=1e-12)    
+    @test sol3.objective ≈ 1.0125 rtol=1e-2
 end
