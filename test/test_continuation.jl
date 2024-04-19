7de1534d895bbec2fde7c56d162fe4c510f3f049
Pierre Martinon <pierrecmartinon@gmail.com>
2024-04-19 14:34:49 +0200

    discrete continuation on vmax and Tmax

--- test/test_continuation.jl
+++ test/test_continuation.jl
@@ -2,10 +2,15 @@ using CTDirect
 using CTBase
 using Printf
 using Statistics
+using Plots; pyplot()
+
+test1 = true
+test2 = true
+test3 = false
 
 #################################################
 # goddard max final altitude
-println("Test: discrete continuation (goddard with speed limit)")
+println("Test: discrete continuation (goddard)")
 
 Cd = 310
 Tmax = 3.5
@@ -37,48 +42,86 @@ dynamics!(ocp, (x, u, v) -> F0(x) + u*F1(x) )
 
 # solve unconstrained problem
 sol0 = solve(ocp, print_level=0)
-@printf("Objective for unconstrained speed %.6f\n", sol0.objective)
+@printf("Objective for reference solution %.6f\n", sol0.objective)
 sol = sol0
 
-# default init
-print("\nSolve for different speed limits, default initial guess\nvmax ")
-iter_list = []
-for vmax=0.15:-0.01:0.01
-    print(vmax," ")
-    remove_constraint!(ocp, :speed_limit)
-    constraint!(ocp, :state, Index(2), 0, vmax, :speed_limit)
-    global sol = solve(ocp, print_level=0)   
-    #@printf("vmax %.2f objective %.6f iterations %d\n",vmax,sol.objective, sol.iterations)
-    push!(iter_list, sol.iterations)
+if test1
+    # default init
+    print("\nSolve for different speed limits, default initial guess\nvmax ")
+    iter_list = []
+    for vmax=0.15:-0.01:0.05
+        print(vmax," ")
+        remove_constraint!(ocp, :speed_limit)
+        constraint!(ocp, :state, Index(2), 0, vmax, :speed_limit)
+        global sol = solve(ocp, print_level=0)   
+        #@printf("vmax %.2f objective %.6f iterations %d\n",vmax,sol.objective, sol.iterations)
+        push!(iter_list, sol.iterations)
+    end
+    @printf("\nAverage iterations %d\n", mean(iter_list))
+
+    # warm start
+    print("\nDiscrete continuation on speed limit, with warm start\nvmax ")
+    vmax_list = []
+    obj_list = []
+    iter_list = []
+    for vmax=0.15:-0.01:0.05
+        print(vmax," ")
+        remove_constraint!(ocp, :speed_limit)
+        constraint!(ocp, :state, Index(2), 0, vmax, :speed_limit)
+        global sol = solve(ocp, print_level=0, init=OptimalControlInit(sol))   
+        #@printf("vmax %.2f objective %.6f iterations %d\n",vmax,sol.objective, sol.iterations)
+        push!(vmax_list, vmax)
+        push!(obj_list, sol.objective)
+        push!(iter_list, sol.iterations)
+    end
+    @printf("\nAverage iterations %d\n", mean(iter_list))
+
+    # plot obj(vmax)
+    pobj = plot(vmax_list, obj_list, label="r(tf)", xlabel="Speed limit (vmax)", ylabel="Maximal altitude r(tf)",seriestype=:scatter)
+
+    # plot multiple solutions
+    plot(sol0)
+    p = plot!(sol)
+
+    display(plot(pobj, p, layout=2))
 end
-@printf("\nAverage iterations %d\n", mean(iter_list))
 
-# warm start
-print("\nDiscrete continuation on speed limit, with warm start\nvmax ")
-vmax_list = []
-obj_list = []
-iter_list = []
-for vmax=0.15:-0.01:0.01
-    print(vmax," ")
+if test2
+    print("\nDiscrete continuation on maximal thrust\nTmax ")
+    # reset speed constraint
     remove_constraint!(ocp, :speed_limit)
+    vmax = 0.1
     constraint!(ocp, :state, Index(2), 0, vmax, :speed_limit)
-    global sol = solve(ocp, print_level=0, init=OptimalControlInit(sol))   
-    #@printf("vmax %.2f objective %.6f iterations %d\n",vmax,sol.objective, sol.iterations)
-    push!(vmax_list, vmax)
-    push!(obj_list, sol.objective)
-    push!(iter_list, sol.iterations)
+    sol = solve(ocp, print_level=0)
+    sol0 = sol
+
+    # continuation on Tmax (using default init is slower)
+    Tmax_list = []
+    obj_list = []
+    for Tmax_local=3.5:-0.5:1
+        global Tmax = Tmax_local 
+        print(Tmax," ")   
+        global sol = solve(ocp, print_level=0, init=OptimalControlInit(sol))
+        push!(Tmax_list, Tmax)
+        push!(obj_list, sol.objective)
+        #print(Tmax, " ", sol.objective, " ", sol.iterations)
+    end
+    
+    # plot obj(vmax)
+    pobj = plot(Tmax_list, obj_list, label="r(tf)", xlabel="Maximal thrust (Tmax)", ylabel="Maximal altitude r(tf)",seriestype=:scatter)
+
+    # plot multiple solutions
+    plot(sol0)
+    p = plot!(sol)
+    
+    display(plot(pobj, p, layout=2, reuse=false))
+
 end
-@printf("\nAverage iterations %d\n", mean(iter_list))
-
-# plot obj(vmax)
-pobj = plot(vmax_list, obj_list, label="r(tf)", xlabel="Speed limit (vmax)", ylabel="Maximal altitude r(tf)",seriestype=:scatter)
-
-# plot multiple solutions
-plot(sol0)
-p = plot!(sol)
-
-display(plot(pobj, p, layout=2))
 
 
-
-# +++ 2ene exemple avec tf fixe qui varie ?
+# currently only one call to time is allowed...
+if test3
+    time!(ocp, 0, 0.1)
+    sol = solve(ocp, print_level=0)
+    println(sol.objective)
+end
