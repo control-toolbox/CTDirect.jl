605330ba08ccd0331aa50ef81f935ba29f20b9d6
Pierre Martinon <pierrecmartinon@gmail.com>
2024-07-11 10:56:48 +0200

    vanderpol

---
+++ problems/vanderpol.jl
@@ -0,0 +1,20 @@
+# Van der Pol example from Bocop
+
+@def vanderpol begin
+    # constants
+    omega = 1
+    epsilon = 1
+
+    t ∈ [ 0, 2 ], time
+    x ∈ R², state
+    u ∈ R, control
+    x(0) == [ 1, 0 ]
+    ẋ(t) == [ x[2](t), 
+            epsilon*omega*(1-x[1](t)^2)*x[2](t) - omega^2*x[1](t) + u(t) ] 
+    ∫(0.5*(x[1](t)^2 + x[2](t)^2 + u(t)^2)) → min
+end
+
+#sol = solve(vanderpol)
+#plot(sol)
+
+return((ocp=vanderpol, obj=1.047921, name="vanderpol"))
