0961bc70ede6ffd1109a95af9922fec98eb6b122
Pierre Martinon <pierrecmartinon@gmail.com>
2024-09-03 09:01:55 +0200

    postponed

--- src/gauss.jl
+++ src/gauss.jl
@@ -9,13 +9,14 @@ Q. if taking controls at stages, how to define the control at 'step' (for path c
 
 # Later adjust this one for generic IRK scheme ! (use stage value)
 # define several tags for different methods
-# use intermediate abstract type ImplicitRungeKuttaTag
+# use intermediate abstract type ImplicitRungeKutta ?
+#+++ add Midpoint_imp
 
 struct GaussLegendre2 <: Discretization
     stage::Int
     additional_controls::Int
-    butcher_a
-    butcher_b
-    butcher_c
+    butcher_a::Matrix{Float64}
+    butcher_b::Vector{Float64}
+    butcher_c::Vector{Float64}
     GaussLegendre2() = new(2, 0, [0.25 (0.25 - sqrt(3)/6); (0.25 + sqrt(3)/6) 0.25], [0.5, 0.5], [(0.5 - sqrt(3)/6), (0.5 + sqrt(3)/6)])
 end
