7ae523430ed0dc21f4953e23329c48bfac5f8991
Pierre Martinon <pierrecmartinon@gmail.com>
2024-09-05 10:13:32 +0200

    adjust prof

--- benchmark/prof.jl
+++ benchmark/prof.jl
@@ -12,10 +12,11 @@ using BenchmarkTools
 using JET
 
 precompile = true
-
+test_objective = true
+test_constraints = true
+test_transcription = true
 test_solve = true
-#test = :objective
-test = :constraints
+
 test_code_warntype = false
 test_jet = false
 
@@ -24,43 +25,53 @@ include("../test/problems/goddard.jl")
 prob = goddard_all()
 ocp = prob[:ocp]
 grid_size = 100
-docp, nlp = direct_transcription(ocp, grid_size = grid_size)
 println("Load problem ", prob[:name])
 
 if precompile
     println("Precompilation")
+    if test_transcription
+        docp, nlp = direct_transcription(ocp, grid_size = grid_size)
+    end
     if test_solve
         direct_solve(ocp, grid_size = grid_size, display = false, max_iter = 2)
     end
-    if test == :objective
+    if test_objective
         CTDirect.DOCP_objective(CTDirect.DOCP_initial_guess(docp), docp)
-    else
+    end
+    if test_constraints
         CTDirect.DOCP_constraints!(zeros(docp.dim_NLP_constraints), CTDirect.DOCP_initial_guess(docp), docp)
     end
 end
 
-if test == :objective
+# evaluation
+if test_objective
     println("Timed objective")
-    #@timev CTDirect.DOCP_objective(CTDirect.DOCP_initial_guess(docp), docp)
     @btime CTDirect.DOCP_objective(CTDirect.DOCP_initial_guess(docp), docp)
-else
-    println("Timed constraints")
-    #@timev CTDirect.DOCP_constraints!(zeros(docp.dim_NLP_constraints), CTDirect.DOCP_initial_guess(docp), docp)    
+end
+if test_constraints
+    println("Timed constraints")   
     @btime CTDirect.DOCP_constraints!(zeros(docp.dim_NLP_constraints), CTDirect.DOCP_initial_guess(docp), docp)
+end
+
+# transcription
+if test_transcription
+    println("Timed transcription")
+    @btime docp, nlp = direct_transcription(ocp, grid_size = grid_size)
 end
 
 # full solve
 if test_solve
     println("Timed full solve")
-    #@timev sol = direct_solve(ocp, grid_size = grid_size, display=false)
     @btime sol = direct_solve(ocp, grid_size = grid_size, display=false)
 end
 
+
 if test_code_warntype
-    if test == :objective
+    if test_objective
         # NB. Pb with the mayer part: obj is type unstable (Any) because ocp.mayer is Union(Mayer,nothing), even for mayer problems (also, we should not even enter this code part for lagrange problems since has_mayer us defined as const in DOCP oO ...).
         @code_warntype CTDirect.DOCP_objective(CTDirect.DOCP_initial_guess(docp), docp)
-    else
+    end
+    if test_constraints
         # OK !
         @code_warntype CTDirect.DOCP_constraints!(
             zeros(docp.dim_NLP_constraints),
@@ -71,11 +82,12 @@ if test_code_warntype
 end
 
 if test_jet
-    if test == :objective
+    if test_objective
         # 4 possible errors
         # due to the ocp.mayer type problem cf above
         @report_opt CTDirect.DOCP_objective(CTDirect.DOCP_initial_guess(docp), docp)
-    else
+    end
+    if test_constraints
         # 50 possible errors: some getindex (Integer vs Int...)
         # all variables x,u,v
         @report_opt CTDirect.DOCP_constraints!(
@@ -85,4 +97,3 @@ if test_jet
         )
     end
 end
-
