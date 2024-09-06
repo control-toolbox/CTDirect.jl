1a01d1a92b5119758c94be76925f19c80a0f9e8b
Olivier Cots <olivier.cots@irit.fr>
2024-09-06 16:52:05 +0200

    add auqa test

---
+++ test/suite/test_aqua.jl
@@ -0,0 +1,13 @@
+println("Test: Aqua")
+
+@testset "Aqua.jl" begin
+    Aqua.test_all(
+        CTDirect;
+        ambiguities = false,
+        #stale_deps=(ignore=[:SomePackage],),
+        deps_compat = (ignore = [:LinearAlgebra, :Unicode],),
+        piracies = true,
+    )
+    # do not warn about ambiguities in dependencies
+    Aqua.test_ambiguities(CTDirect)
+end
