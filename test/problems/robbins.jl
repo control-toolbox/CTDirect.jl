302d6229d780b70d6f389da149e14d5d93a118fa
Pierre Martinon <pierrecmartinon@gmail.com>
2025-01-08 17:51:29 +0100

    added jackson, robbins, vanderpol

--- test/problems/robbins.jl
+++ test/problems/robbins.jl
@@ -1,5 +1,33 @@
 # Robbins example from Bocop
-function robbins()
+function robbins_model()
+    # constants
+    alpha = 3
+    beta = 0
+    gamma = 0.5    
+    pre_ocp = CTModels.PreModel()
+    CTModels.state!(pre_ocp, 3)
+    CTModels.control!(pre_ocp, 1)
+    CTModels.time!(pre_ocp, t0=0.0, tf=10)
+    CTModels.constraint!(pre_ocp, :state, rg=1:1, lb=[0], ub=[Inf], label=:state_rg)
+    function bc!(r, x0, xf, v)
+        r[1:3] .= x0
+        r[4:6] .= xf
+    end
+    CTModels.constraint!(pre_ocp, :boundary, f=bc!, lb=[1, -2, 0, 0, 0, 0], ub=[1, -2, 0, 0, 0, 0], label=:boundary)
+    function f!(r, t, x, u, v)
+        r[1] = x[2]
+        r[2] = x[3]
+        r[3] = u[1]
+    end
+    CTModels.dynamics!(pre_ocp, f!)
+    l(t, x, u, v) = alpha * x[1] + beta * x[1]^2 + gamma * u[1]^2
+    CTModels.objective!(pre_ocp, :min, lagrange=l)
+    CTModels.definition!(pre_ocp, Expr(:robbins))
+    ocp = CTModels.build_model(pre_ocp)
+    return ocp
+end
+
+#=function robbins()
     @def robbins begin
         # constants
         alpha = 3
@@ -17,4 +45,4 @@ function robbins()
     end
 
     return ((ocp = robbins, obj = 20.0, name = "robbins", init = nothing))
-end
+end=#
