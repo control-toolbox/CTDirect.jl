f2868f31554e126a57688505ebea586578b491f8
Pierre Martinon <pierrecmartinon@gmail.com>
2025-01-30 16:07:53 +0100

    ready to merge

--- src/docp.jl
+++ src/docp.jl
@@ -586,3 +586,30 @@ function get_time_grid(xu, docp::DOCP{<:Discretization, <: ScalVect, <: ScalVect
     @. grid = t0 + docp.NLP_normalized_time_grid * (tf - t0)
     return grid
 end
+
+
+"""
+$(TYPEDSIGNATURES)
+
+Set optimization variables in the NLP variables (for initial guess)
+"""
+function set_optim_variable!(xu, v_init, docp)
+    xu[(end - docp.dim_NLP_v + 1):end] .= v_init
+end
+
+"""
+$(TYPEDSIGNATURES)
+
+Build full, ordered sets of bounds for state, control or optimization variables
+"""
+function build_bounds(dim_var, dim_box, box_triplet)
+    x_lb = -Inf * ones(dim_var)
+    x_ub = Inf * ones(dim_var)
+    for j = 1:(dim_box)
+        indice = box_triplet[2][j]
+        x_lb[indice] = box_triplet[1][j]
+        x_ub[indice] = box_triplet[3][j]
+    end
+
+    return x_lb, x_ub
+end
