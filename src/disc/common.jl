5bbbb0368517b29dc0e0b106a7088cb1552f7d23
Pierre Martinon <pierrecmartinon@gmail.com>
2025-03-14 17:00:42 +0100

    disc

---
+++ src/disc/common.jl
@@ -0,0 +1,191 @@
+#= Common parts for the discretization =#
+
+
+"""
+$(TYPEDSIGNATURES)
+
+Retrieve optimization variables from the NLP variables.
+Convention: stored at the end, hence not dependent on the discretization method
+Scalar / Vector output
+"""
+function get_OCP_variable(xu, docp::DOCP{<: Discretization, <: ScalVect, <: ScalVect, ScalVariable})
+    return xu[docp.dim_NLP_variables]
+end
+function get_OCP_variable(xu, docp::DOCP{<: Discretization, <: ScalVect, <: ScalVect, VectVariable})
+    return @view xu[(docp.dim_NLP_variables - docp.dim_NLP_v + 1):docp.dim_NLP_variables]
+end
+
+
+"""
+$(TYPEDSIGNATURES)
+
+Retrieve state variables at given time step from the NLP variables.
+Convention: 1 <= i <= dim_NLP_steps+1
+Scalar / Vector output
+"""
+function get_OCP_state_at_time_step(xu, docp::DOCP{<: Discretization, ScalVariable, <: ScalVect, <: ScalVect}, i)
+    offset = (i-1) * docp.discretization._step_variables_block
+    return xu[offset+1]
+end
+function get_OCP_state_at_time_step(xu, docp::DOCP{<: Discretization, VectVariable, <: ScalVect, <: ScalVect}, i)
+    offset = (i-1) * docp.discretization._step_variables_block
+    return @view xu[(offset + 1):(offset + docp.dim_OCP_x)]
+end
+
+"""
+$(TYPEDSIGNATURES)
+
+Retrieve state variable for lagrange cost at given time step from the NLP variables.
+Convention: 1 <= i <= dim_NLP_steps+1
+"""
+function get_lagrange_state_at_time_step(xu, docp::DOCP{<: Discretization}, i)
+    offset = (i-1) * docp.discretization._step_variables_block
+    return xu[offset + docp.dim_NLP_x]
+end
+
+
+"""
+$(TYPEDSIGNATURES)
+
+Retrieve control variables at given time step from the NLP variables.
+Convention: 1 <= i <= dim_NLP_steps, with convention u(tf) = U_N
+Scalar / Vector output
+"""
+function get_OCP_control_at_time_step(xu, docp::DOCP{<: Discretization, <: ScalVect, ScalVariable, <: ScalVect}, i)
+    # final time case
+    (i == docp.dim_NLP_steps + 1) && (i = docp.dim_NLP_steps)
+    offset = (i-1) * docp.discretization._step_variables_block + docp.dim_NLP_x
+    return xu[offset+1]
+end
+function get_OCP_control_at_time_step(xu, docp::DOCP{<: Discretization, <: ScalVect, VectVariable, <: ScalVect}, i)
+    # final time case
+    (i == docp.dim_NLP_steps + 1) && (i = docp.dim_NLP_steps)
+    offset = (i-1) * docp.discretization._step_variables_block + docp.dim_NLP_x
+    return @view xu[(offset + 1):(offset + docp.dim_NLP_u)]
+end
+
+
+"""
+$(TYPEDSIGNATURES)
+
+Retrieve stage variables at given time step/stage from the NLP variables.
+Convention: 1 <= i <= dim_NLP_steps(+1),	1 <= j <= s
+Vector output
+Note that passing correct indices is up to the caller, no checks are made here.
+"""
+function get_stagevars_at_time_step(xu, docp::DOCP, i, j)
+    offset = (i-1) * docp.discretization._step_variables_block + docp.dim_NLP_x + docp.dim_NLP_u + (j-1)*docp.dim_NLP_x
+    return @view xu[(offset + 1):(offset + docp.dim_NLP_x)]
+end
+
+
+"""
+$(TYPEDSIGNATURES)
+
+Set optimization variables in the NLP variables (for initial guess)
+"""
+function set_optim_variable!(xu, v_init, docp)
+    xu[(end - docp.dim_NLP_v + 1):end] .= v_init
+end
+
+
+"""
+$(TYPEDSIGNATURES)
+
+Set initial guess for state variables at given time step
+Convention: 1 <= i <= dim_NLP_steps+1
+"""
+function set_state_at_time_step!(xu, x_init, docp::DOCP{<: Discretization}, i)
+    # initialize only actual state variables from OCP (not lagrange state)
+    if !isnothing(x_init)
+        offset = (i-1) * docp.discretization._step_variables_block
+        xu[(offset + 1):(offset + docp.dim_OCP_x)] .= x_init
+    end
+end
+
+# +++ add here a more toplevel set_variables_at_time_step ? (potentially include stage variables later ?) and call it from docp
+"""
+$(TYPEDSIGNATURES)
+
+Set initial guess for control variables at given time step
+Convention: 1 <= i <= dim_NLP_steps
+"""
+function set_control_at_time_step!(xu, u_init, docp::DOCP{<: Discretization}, i)
+    if !isnothing(u_init)
+        if i <= docp.dim_NLP_steps
+            offset = (i-1) * docp.discretization._step_variables_block + docp.dim_NLP_x
+            xu[(offset + 1):(offset + docp.dim_NLP_u)] .= u_init
+        end
+    end
+end
+
+
+"""
+$(TYPEDSIGNATURES)
+
+Set work array for all dynamics and lagrange cost evaluations
+"""
+function setWorkArray(docp::DOCP{<: Discretization}, xu, time_grid, v)
+    return nothing
+end
+
+
+"""
+$(TYPEDSIGNATURES)
+
+Build sparsity pattern for Jacobian of constraints
+"""
+function DOCP_Jacobian_pattern(docp::DOCP{D}) where (D <: Discretization)
+    error("DOCP_Jacobian_pattern not implemented for discretization ", D)
+end
+
+
+"""
+$(TYPEDSIGNATURES)
+
+Build sparsity pattern for Hessian of Lagrangian
+"""
+function DOCP_Hessian_pattern(docp::DOCP{D}) where (D <: Discretization)
+    error("DOCP_Hessian_pattern not implemented for discretization ", D)
+end
+
+
+
+
+"""
+$(TYPEDSIGNATURES)
+
+Add block of nonzeros elements to a sparsity pattern 
+Format: boolean matrix (M) or index vectors (Is, Js) 
+Includes a more compact method for single element case
+Option to add the symmetric block also (eg for Hessian)
+Note: independent from discretization scheme
+"""
+function add_nonzero_block!(M, i_start, i_end, j_start, j_end; sym=false)
+    M[i_start:i_end, j_start:j_end] .= true
+    sym && (M[j_start:j_end, i_start:i_end] .= true)
+    return
+end
+function add_nonzero_block!(M, i, j; sym=false)
+    M[i,j] = true
+    sym && (M[j,i] = true)
+    return
+end
+function add_nonzero_block!(Is, Js, i_start, i_end, j_start, j_end; sym=false)
+    for i=i_start:i_end
+        for j=j_start:j_end
+            push!(Is, i)
+            push!(Js, j)
+            sym && push!(Is, j)
+            sym && push!(Js, i)
+        end
+    end
+    return
+end
+function add_nonzero_block!(Is, Js, i, j; sym=false)
+    push!(Is, i)
+    push!(Js, j)
+    sym && push!(Is, j)
+    sym && push!(Js, i)
+    return
+end
---
+++ src/disc/euler.jl
@@ -0,0 +1,150 @@
+#= Functions for explicit and implicit euler discretization scheme
+Internal layout for NLP variables: 
+[X_1,U_1,K_1 .., X_N,U_N,K_N, X_N+1, V]
+with the convention 
+- Explicit Euler: u([t_i,t_i+1[) = U_i and u(tf) = U_N
+- Implicit Euler: u(]t_i,t_i+1]) = U_i and u(t0) = U_1
+Note that both the explicit and implicit versions therefore use the same variables layout.
+=#
+
+struct Euler <: Discretization
+
+    info::String
+    _step_variables_block::Int
+    _state_stage_eqs_block::Int
+    _step_pathcons_block::Int
+    _final_control::Bool
+    _explicit::Bool
+
+    # constructor
+    function Euler(dim_NLP_steps, dim_NLP_x, dim_NLP_u, dim_NLP_v, dim_u_cons, dim_x_cons, dim_xu_cons, dim_boundary_cons, dim_v_cons; explicit=true)
+
+        # aux variables
+        step_variables_block = dim_NLP_x + dim_NLP_u
+        state_stage_eqs_block = dim_NLP_x
+        step_pathcons_block = dim_u_cons + dim_x_cons + dim_xu_cons
+
+        # NLP variables size ([state, control]_1..N, final state, variable)
+        dim_NLP_variables = dim_NLP_steps * step_variables_block + dim_NLP_x + dim_NLP_v
+        
+        # NLP constraints size ([dynamics, path]_1..N, final path, boundary, variable)
+        dim_NLP_constraints = dim_NLP_steps * (state_stage_eqs_block + step_pathcons_block) + step_pathcons_block + dim_boundary_cons + dim_v_cons
+
+        if explicit 
+            info = "Euler (explicit), 1st order"
+        else
+            info = "Euler (implicit), 1st order"
+        end
+        disc = new(info, step_variables_block, state_stage_eqs_block, step_pathcons_block, false, explicit)
+
+        return disc, dim_NLP_variables, dim_NLP_constraints
+    end
+end
+
+
+"""
+$(TYPEDSIGNATURES)
+
+Retrieve control variables at given time step from the NLP variables.
+Convention: see above for acplicit / implicit versions
+Scalar / Vector output
+"""
+function get_OCP_control_at_time_step(xu, docp::DOCP{Euler, <: ScalVect, ScalVariable, <: ScalVect}, i)
+    if docp.discretization._explicit
+        # final time case
+        (i == docp.dim_NLP_steps + 1) && (i = docp.dim_NLP_steps)
+        offset = (i-1) * docp.discretization._step_variables_block + docp.dim_NLP_x
+    else
+        # initial time case
+        (i == 1) && (i = 2)
+        offset = (i-2) * docp.discretization._step_variables_block + docp.dim_NLP_x
+    end
+    return xu[offset+1]
+end
+function get_OCP_control_at_time_step(xu, docp::DOCP{Euler, <: ScalVect, VectVariable, <: ScalVect}, i)
+    if docp.discretization._explicit
+        # final time case
+        (i == docp.dim_NLP_steps + 1) && (i = docp.dim_NLP_steps)
+        offset = (i-1) * docp.discretization._step_variables_block + docp.dim_NLP_x
+    else 
+        # initial time case
+        (i == 1) && (i = 2)
+        offset = (i-2) * docp.discretization._step_variables_block + docp.dim_NLP_x
+    end
+    return @view xu[(offset + 1):(offset + docp.dim_NLP_u)]
+end
+
+
+"""
+$(TYPEDSIGNATURES)
+
+Set work array for all dynamics and lagrange cost evaluations
+"""
+function setWorkArray(docp::DOCP{Euler}, xu, time_grid, v)
+
+    work = similar(xu, docp.dim_NLP_x * docp.dim_NLP_steps)
+
+    # loop over time steps
+    for i = 1:docp.dim_NLP_steps
+        offset = (i-1) * docp.dim_NLP_x
+
+        # get variables at t_i or t_i+1
+        if docp.discretization._explicit
+            index = i
+        else
+            index = i+1
+        end
+        t = time_grid[index]
+        x = get_OCP_state_at_time_step(xu, docp, index)
+        u =get_OCP_control_at_time_step(xu, docp, index)
+
+        # OCP dynamics
+        docp.ocp.dynamics((@view work[offset+1:offset+docp.dim_OCP_x]), t, x, u, v)
+        # lagrange cost
+        if docp.is_lagrange
+            docp.ocp.lagrange((@view work[offset+docp.dim_NLP_x:offset+docp.dim_NLP_x]), t, x, u, v)
+        end   
+    end
+    return work
+end
+
+
+"""
+$(TYPEDSIGNATURES)
+
+Set the constraints corresponding to the state equation
+Convention: 1 <= i <= dim_NLP_steps+1
+"""
+function setStepConstraints!(docp::DOCP{Euler}, c, xu, v, time_grid, i, work)
+    
+    # offset for previous steps
+    offset = (i-1)*(docp.discretization._state_stage_eqs_block + docp.discretization._step_pathcons_block)
+
+    # 0. variables
+    ti = time_grid[i]
+    xi = get_OCP_state_at_time_step(xu, docp, i)
+    ui = get_OCP_control_at_time_step(xu, docp, i)
+
+    # 1. state equation
+    if i <= docp.dim_NLP_steps
+        # more variables
+        tip1 = time_grid[i+1]
+        xip1 = get_OCP_state_at_time_step(xu, docp, i+1)
+        hi = tip1 - ti
+        offset_dyn_i = (i-1)*docp.dim_NLP_x
+
+        # state equation: euler rule
+        @views @. c[offset+1:offset+docp.dim_OCP_x] = xip1 - (xi + hi * work[offset_dyn_i+1:offset_dyn_i+docp.dim_OCP_x])
+        if docp.is_lagrange
+            c[offset+docp.dim_NLP_x] = get_lagrange_state_at_time_step(xu, docp, i+1) - (get_lagrange_state_at_time_step(xu, docp, i) + hi * work[offset_dyn_i+docp.dim_NLP_x])
+        end
+        offset += docp.dim_NLP_x
+
+    end
+   
+    # 2. path constraints
+    if docp.discretization._step_pathcons_block > 0
+        setPathConstraints!(docp, c, ti, xi, ui, v, offset)
+    end
+    
+end
