91835bfea3d43450bb186a1ce7389feb30bd8e95
Jean-Baptiste Caillau <jean-baptiste.caillau@univ-cotedazur.fr>
2025-06-13 14:56:19 +0200

    beam2 with :trapeze OK

--- test/suite/test_exa.jl
+++ test/suite/test_exa.jl
@@ -11,30 +11,36 @@ else
     exa_backend = nothing 
 end
 
-# beam
+# beam2
 if !isdefined(Main, :beam2)
     include("../problems/beam2.jl")
 end
-# cpu case
+
 @testset verbose = true showtiming = true ":examodel :cpu :beam2" begin
     prob = beam2()
-    sol = solve(prob.ocp, :madnlp, :exa; disc_method = :euler, display=true)
+    sol = solve(prob.ocp, :madnlp, :exa; disc_method = :trapeze, display = true)
     @test sol.objective ≈ prob.obj rtol = 1e-2
 end
-#=@testset verbose = true showtiming = true ":examodel :cpu :beam2 :init" begin
-    prob = beam2()
-    sol = solve(prob.ocp, :madnlp, :exa; disc_method = :euler, display=true, init=(control=0.0,), max_iter = 0)
-    # default init would be 0.1
-    @test control(sol)(1) == 0e0
-end=#
 
-# gpu case
 if !isnothing(exa_backend) 
 @testset verbose = true showtiming = true ":examodel :GPU :beam2" begin
     prob = beam2()
-    sol = solve(prob.ocp, :madnlp, :exa; disc_method = :euler, exa_backend = exa_backend, display=false)
+    sol = solve(prob.ocp, :madnlp, :exa; disc_method = :trapeze, exa_backend = exa_backend, display = true)
     @test sol.objective ≈ prob.obj rtol = 1e-2
 end
 else
     println("skip GPU test")
 end
+
+@ignore begin # debug
+# swimmer2
+if !isdefined(Main, :swimmer2)
+    include("../problems/swimmer2.jl")
+end
+
+@testset verbose = true showtiming = true ":examodel :cpu :swimmer2" begin
+    prob = swimmer2()
+    sol = solve(prob.ocp, :madnlp, :exa; disc_method = :trapeze, display = true)
+    @test sol.objective ≈ prob.obj rtol = 1e-2
+end
+end # debug
