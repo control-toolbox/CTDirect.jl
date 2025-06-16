4576cd97ca610f442b4b1541dfaffebce977fca8
Pichon <tatam.pichon@gmail.com>
2025-06-16 14:34:49 +0200

    tmp

---
+++ tmp/vanderpol.jl
@@ -0,0 +1,24 @@
+using Pkg
+Pkg.activate("tmp")
+
+using CTDirect: CTDirect, solve, direct_transcription, set_initial_guess, build_OCP_solution
+using CTModels: CTModels, objective, state, control, variable, costate, time_grid, iterations
+using CTParser: CTParser, @def, prefix!
+prefix!(:CTModels) # tell CTParser def macro to use CTModels instead of OptimalControl
+
+using NLPModelsIpopt
+using Plots
+
+
+include("../test/problems/vanderpol.jl")
+problem = vanderpol()
+
+
+sol = solve(problem[:ocp], grid_size=500, disc_method= :trapeze, display = false)
+#@btime direct_sol = solve(ocp; grid_size=500, disc_method = :trapeze, display = false)
+display(plot(sol))
+
+solmid = solve(problem[:ocp], grid_size=500, disc_method= :midpoint, display = false)
+display(plot(solmid))
+
+
