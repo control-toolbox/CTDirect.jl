84aae27731c2aca7b0e08ee0cb5b4751adc7817b
Pierre Martinon <pierrecmartinon@gmail.com>
2025-06-24 12:05:25 +0200

    0.15 (#421)
    * updated docstrings; reworked some parameters passing
    
    * NLP getter is now `model(docp)`
    
    * solver infos update
    
    * disable moonshot workflow; more docstrings
    
    * up moonshot
    
    * up trigger ext
    
    * add compat Project.toml
    
    ---------
    
    Co-authored-by: Olivier Cots <olivier.cots@irit.fr>

--- src/solve.jl
+++ src/solve.jl
@@ -52,7 +52,7 @@ const WEAKDEPS = Dict{Type, Any}(
     KnitroBackend => [:NLPModelsKnitro],
     # NLP modeller
     ADNLPBackend  => [:ADNLPModels],
-    ExaBackend    => [:ExaModels, :MadNLPGPU, :CUDA],
+    ExaBackend    => [:ExaModels],
 )
 
 # solver
@@ -125,10 +125,10 @@ Other keywords are passed down to the NLP modeler and solver.
 * `state(sol)`, `control(sol)`: functions for state and control variables (trajectory)
 * `variable(sol)`: optimization variables if any (e.g. free final time)
 * `successful(sol)`: boolean indicating successful convergence of the NLP solver
-* `status(sol)`: integer for the return code of the NLP solver
-* `message(sol)`: string returned by the NLP solver, if any
-* `constraints_violation(sol)`: primal feasibility from the NLP solver
-* `iterations(sol)`: number of iterations from the NLP solver
+* `status(sol)`: symbol for the return code of the NLP solver
+* `message(sol)`: string with specific info from the NLP solver, if any
+* `constraints_violation(sol)`: primal feasibility at the solution
+* `iterations(sol)`: number of iterations 
 """
 function solve(
     ocp::CTModels.Model,
