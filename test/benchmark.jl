74753ecad3dca2acd71c673b2f7e7f8d1e0c40a6
Pierre Martinon <pierrecmartinon@gmail.com>
2025-07-04 17:47:52 +0200

    bench

--- test/benchmark.jl
+++ test/benchmark.jl
@@ -24,36 +24,39 @@ for problem_file in filter(contains(r".jl$"), readdir(problem_path; join=true))
     include(problem_file)
 end
 
-function bench_list(problem_list; verbose=1, nlp_solver, kwargs...)
 
-    if verbose > 3
+# solve list of problems, for a given grid size and other options
+# verbose <= 1: no output
+# verbose > 1: print summary (iter, obj, time)
+# verbose > 2: print NLP iterations also
+function bench_problem(problem; verbose=1, nlp_solver, kwargs...)
+
+    if verbose > 2
         display = true
     else
         display = false
     end
 
-    # solve examples with timer and objective check
-    t_list = []
-    for problem in problem_list
-
-        # check (will also precompile)
-        sol = solve(problem[:ocp], nlp_solver; init=problem[:init], display=display, kwargs...)
-        if !isnothing(problem[:obj]) && !isapprox(objective(sol), problem[:obj], rtol=5e-2)
-            error("Objective mismatch for ", problem[:name], ": ", objective(sol), " instead of ", problem[:obj])
-        else
-            verbose > 2 && @printf("%-30s: %4d iter %5.2f obj ", problem[:name], iterations(sol), objective(sol))
-        end
-
-        # time
-        t = @belapsed solve($problem[:ocp], $nlp_solver; init=$problem[:init], display=false, $kwargs...)
-        append!(t_list, t)
-        verbose > 2 && @printf("%7.2f s\n", t)
+    # check (will also precompile)
+    sol = solve(problem[:ocp], nlp_solver; init=problem[:init], display=display, kwargs...)
+    if !isnothing(problem[:obj]) && !isapprox(objective(sol), problem[:obj], rtol=5e-2)
+        verbose > 1 && println("\nObjective mismatch for ", problem[:name], ": ", objective(sol), " instead of ", problem[:obj])
+        success = false
+    else
+        verbose > 1 && @printf("\n%-20s: %4d iter %5.2f obj ", problem[:name], iterations(sol), objective(sol))
+        success = true
     end
+    iter = iterations(sol)
 
-    return sum(t_list)
+    # time
+    time = @belapsed solve($problem[:ocp], $nlp_solver; init=$problem[:init], display=false, $kwargs...)
+    verbose > 1 && @printf("%7.2f s\n", time)
+    
+    return time, iter, success
 end
 
 
+# perform benchmark
 function bench(;verbose=1, 
     target_list=:default,
     grid_size_list=[250, 500, 1000, 2500, 5000],
@@ -84,21 +87,60 @@ function bench(;verbose=1,
     elseif target_list == :hard
         target_list = ["algal_bacterial", "bioreactor_1day", "bioreactor_Ndays", "bolza_freetf", "insurance", "swimmer"]
     end
-    verbose > 1 && println("\nProblem list: ", target_list)
+    verbose > 2 && println("\nProblem list: ", target_list)
     problem_list = []
     for problem_name in target_list
         ocp_data = getfield(Main, Symbol(problem_name))()
         push!(problem_list, ocp_data)
     end
 
-    verbose > 1 && println("\nGrid size list: ", grid_size_list)
-    t_list = []
-    for grid_size in grid_size_list
-        t = bench_list(problem_list; grid_size=grid_size, verbose=verbose, nlp_solver=nlp_solver, kwargs...)
-        append!(t_list, t)
-        @printf("Grid size %6d: time (s) = %6.1f\n", grid_size, t)
+    # solve problem list for all grid sizes
+    verbose > 2 && println("\nGrid size list: ", grid_size_list)
+    t_bench = []
+    i_bench = []
+    s_bench = []
+    for problem in problem_list
+        verbose > 1 && @printf("\nTesting problem %-20s for grid size ", problem[:name])
+        t_list = []
+        i_list = []
+        s_list = []
+        for grid_size in grid_size_list
+            verbose > 1 && @printf("%d ", grid_size)
+            time, iter, success = bench_problem(problem; grid_size=grid_size, verbose=verbose-1, nlp_solver=nlp_solver, kwargs...)
+            append!(t_list, time)
+            append!(i_list, iter)
+            append!(s_list, success)
+        end
+        push!(t_bench, t_list)
+        push!(i_bench, i_list)
+        push!(s_bench, s_list)
     end
-    #return t_list
+
+    # display: 1 row per problem with (t,i,s) for each grid size in columns
+    # plus last row for total over problem set
+    if verbose > 0
+        i = 1
+        for problem in problem_list
+            @printf("\n%-20s ", problem[:name])
+            for j=1:length(grid_size_list)
+                if s_bench[i][j]
+                    @printf("%4.1f (%4d) ", t_bench[i][j], i_bench[i][j])
+                else
+                    @printf("FAIL     ")
+                end
+            end
+            i = i + 1
+        end
+    end
+    #+++total, quid des fail ?
+    println(t_bench)
+    println(i_bench)
+    println(i_bench[:][1])
+    @printf("\nBENCHMARK             ")
+    #for j=1:length(grid_size_list)
+    #    @printf("%4.1f (%4d)   ", sum(t_bench[:][j]), sum(i_bench[:][j]))
+    #end
+    return
 end
 
 
