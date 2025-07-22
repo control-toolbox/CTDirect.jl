cfe7f60adaebb0e5f7385b55640d1f988d23d7b0
Jean-Baptiste Caillau <jean-baptiste.caillau@univ-cotedazur.fr>
2025-07-22 23:38:27 +0200

    added goddard2 test for :exa

--- test/test_exa.jl
+++ test/test_exa.jl
@@ -10,32 +10,46 @@ if !isdefined(Main, :beam2)
     include("./problems/beam2.jl")
 end
 
+if !isdefined(Main, :goddard2)
+    include("./problems/goddard2.jl")
+end
+
 # test_exa for all backends (CPU + GPU)
 
 function test_exa(exa_backend)
 
-    @testset verbose = true showtiming = true ":examodel :euler" begin
+    # beam2
+
+    @testset verbose = true showtiming = true "beam2 :examodel :euler" begin
         prob = beam2()
         sol = solve(prob.ocp, :madnlp, :exa; disc_method = :euler, exa_backend = exa_backend, display = display)
         @test sol.objective ≈ prob.obj rtol = 1e-2
     end
 
-    @testset verbose = true showtiming = true ":examodel :trapeze" begin
+    @testset verbose = true showtiming = true "beam2 :examodel :trapeze" begin
         prob = beam2()
         sol = solve(prob.ocp, :madnlp, :exa; disc_method = :trapeze, exa_backend = exa_backend, display = display)
         @test sol.objective ≈ prob.obj rtol = 1e-2
     end
 
-    @testset verbose = true showtiming = true ":examodel :trapeze :grid_size" begin
+    @testset verbose = true showtiming = true "beam2 :examodel :trapeze :grid_size" begin
         prob = beam2()
         sol = solve(prob.ocp, :madnlp, :exa; disc_method = :trapeze, exa_backend = exa_backend, display = display, grid_size = 1000)
         @test sol.objective ≈ prob.obj rtol = 1e-2
     end
 
-    @testset verbose = true showtiming = true ":examodel :trapeze :init" begin
+    @testset verbose = true showtiming = true "beam2 :examodel :trapeze :init" begin
         prob = beam2()
         sol = solve(prob.ocp, :madnlp, :exa; disc_method = :trapeze, exa_backend = exa_backend, display = display, init=(control=6.66,), max_iter = 0)
         @test control(sol)(0.5) == 6.66
+    end
+
+    # goddard2
+
+    @testset verbose = true showtiming = true "goddard2 :examodel :trapeze :grid_size" begin
+        prob = goddard2()
+        sol = solve(prob.ocp, :madnlp, :exa; disc_method = :trapeze, exa_backend = exa_backend, display = display, grid_size = 1000)
+        @test sol.objective ≈ prob.obj rtol = 1e-2
     end
     
 end
