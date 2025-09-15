c4c12b85f834508f1ad7e046758f9218219ff9e4
Pierre Martinon <pierrecmartinon@gmail.com>
2025-09-15 12:54:33 +0200

    split parse_description for nlp solver / model; cleaned ADNLPModels part

--- ext/CTDirectExtADNLP.jl
+++ ext/CTDirectExtADNLP.jl
@@ -21,24 +21,23 @@ function CTDirect.build_nlp!(
     x0;
     adnlp_backend=CTDirect.__adnlp_backend(),
     show_time=false, #+default
-    matrix_free=false, #+default
-    nlp_solver=nothing,
     kwargs...,
 )
-
-    # +++ minimize[=true], set to false if flags max and not max_to_min
 
     # redeclare objective and constraints functions
     f = x -> CTDirect.DOCP_objective(x, docp)
     c! = (c, x) -> CTDirect.DOCP_constraints!(c, x, docp)
 
-    # +++ todo: use a variable list of kwargs in a single constructor call and splat
-    # backend_options
+    # unused backends (option excluded_backend = [:jprod_backend, :jtprod_backend, :hprod_backend, :ghjvprod_backend] does not seem to work)
+    unused_backends = (hprod_backend=ADNLPModels.EmptyADbackend,
+                jtprod_backend=ADNLPModels.EmptyADbackend,
+                jprod_backend=ADNLPModels.EmptyADbackend,
+                ghjvprod_backend=ADNLPModels.EmptyADbackend)
 
     # call NLP problem constructor
     if adnlp_backend == :manual
 
-        # build sparsity pattern
+        # build sparsity patterns for Jacobian and Hessian
         J_backend = ADNLPModels.SparseADJacobian(
             docp.dim_NLP_variables,
             f,
@@ -53,90 +52,33 @@ function CTDirect.build_nlp!(
             c!,
             CTDirect.DOCP_Hessian_pattern(docp),
         )
+        backend_options = (gradient_backend=ADNLPModels.ReverseDiffADGradient,
+                jacobian_backend=J_backend,
+                hessian_backend=H_backend)
 
-        # build NLP with given patterns; disable unused backends according to solver info
-        if (
-            nlp_solver isa CTDirect.IpoptBackend ||
-            nlp_solver isa CTDirect.MadNLPBackend ||
-            nlp_solver isa CTDirect.KnitroBackend
-        )
-            nlp = ADNLPModel!(
-                f,
-                x0,
-                docp.bounds.var_l,
-                docp.bounds.var_u,
-                c!,
-                docp.bounds.con_l,
-                docp.bounds.con_u;
-                minimize=!docp.flags.max,
-                gradient_backend=ADNLPModels.ReverseDiffADGradient,
-                jacobian_backend=J_backend,
-                hessian_backend=H_backend,
-                hprod_backend=ADNLPModels.EmptyADbackend,
-                jtprod_backend=ADNLPModels.EmptyADbackend,
-                jprod_backend=ADNLPModels.EmptyADbackend,
-                ghjvprod_backend=ADNLPModels.EmptyADbackend,
-                show_time=show_time,
-                #excluded_backend = [:jprod_backend, :jtprod_backend, :hprod_backend, :ghjvprod_backend]
-            )
-        else
-            nlp = ADNLPModel!(
-                f,
-                x0,
-                docp.bounds.var_l,
-                docp.bounds.var_u,
-                c!,
-                docp.bounds.con_l,
-                docp.bounds.con_u;
-                minimize=!docp.flags.max,
-                gradient_backend=ADNLPModels.ReverseDiffADGradient,
-                jacobian_backend=J_backend,
-                hessian_backend=H_backend,
-                show_time=show_time,
-            )
-        end
     else
-        # build NLP; disable unused backends according to solver info
-        if (
-            nlp_solver isa CTDirect.IpoptBackend ||
-            nlp_solver isa CTDirect.MadNLPBackend ||
-            nlp_solver isa CTDirect.KnitroBackend
-        )
-            nlp = ADNLPModel!(
-                f,
-                x0,
-                docp.bounds.var_l,
-                docp.bounds.var_u,
-                c!,
-                docp.bounds.con_l,
-                docp.bounds.con_u;
-                minimize=!docp.flags.max,
-                backend=adnlp_backend,
-                hprod_backend=ADNLPModels.EmptyADbackend,
-                jtprod_backend=ADNLPModels.EmptyADbackend,
-                jprod_backend=ADNLPModels.EmptyADbackend,
-                ghjvprod_backend=ADNLPModels.EmptyADbackend,
-                show_time=show_time,
-            )
-        else
-            # use manual settings including matrix_free
-            nlp = ADNLPModel!(
-                f,
-                x0,
-                docp.bounds.var_l,
-                docp.bounds.var_u,
-                c!,
-                docp.bounds.con_l,
-                docp.bounds.con_u;
-                minimize=!docp.flags.max,
-                backend=adnlp_backend,
-                show_time=show_time,
-                matrix_free=matrix_free,
-            )
-        end
+        # use backend preset
+        backend_options = (backend=adnlp_backend,)
     end
 
+    # build NLP
+    nlp = ADNLPModel!(
+        f,
+        x0,
+        docp.bounds.var_l,
+        docp.bounds.var_u,
+        c!,
+        docp.bounds.con_l,
+        docp.bounds.con_u;
+        minimize=!docp.flags.max,
+        backend_options...,
+        unused_backends...,
+        show_time=show_time,
+    )
+
+    # set NLP in DOCP
     docp.nlp = nlp
+
     return nothing
 end
 
--- src/solve.jl
+++ src/solve.jl
@@ -52,38 +52,44 @@ Parse problem description to retrieve NLP model and solver choice
 - NLP solver: `ipopt`, `madnlp` or `knitro` 
 - NLP model: `:adnlp` or `:exa`
 """
-function parse_description(description)
+function parse_description(description, info)
 
     # default: Ipopt, ADNLPModels
     method = CTBase.complete(description; descriptions=available_methods())
 
-    # get NLP solver choice
-    if :ipopt ∈ method
-        nlp_solver = CTDirect.IpoptBackend()
-    elseif :madnlp ∈ method
-        nlp_solver = CTDirect.MadNLPBackend()
-    elseif :knitro ∈ method
-        nlp_solver = CTDirect.KnitroBackend()
+    if info == :solver
+        # get NLP solver choice
+        if :ipopt ∈ method
+            nlp_solver = CTDirect.IpoptBackend()
+        elseif :madnlp ∈ method
+            nlp_solver = CTDirect.MadNLPBackend()
+        elseif :knitro ∈ method
+            nlp_solver = CTDirect.KnitroBackend()
+        else
+            error("no known solver (:ipopt, :madnlp, :knitro) in method", method)
+        end
+
+        # patch: replaces ipopt by madnlp for :exa as long as the issue with getters for a posteriori treatment is not fixed
+        if (:exa ∈ method) && (:ipopt ∈ method)
+            nlp_solver = CTDirect.MadNLPBackend()
+            @warn "currently replacing Ipopt with MadNLP for :exa"
+        end
+        return nlp_solver
+
+    elseif info == :model
+        # get NLP model choice
+        if :adnlp ∈ method
+            nlp_model = CTDirect.ADNLPBackend()
+        elseif :exa ∈ method
+            nlp_model = CTDirect.ExaBackend()
+        else
+            error("no known model (:adnlp, :exa) in method", method)
+        end
+        return nlp_model
     else
-        error("no known solver (:ipopt, :madnlp, :knitro) in method", method)
+        error("parse_description info should be :solver or :model, not ", info)
+        return
     end
-
-    # get NLP model choice
-    if :adnlp ∈ method
-        nlp_model = CTDirect.ADNLPBackend()
-    elseif :exa ∈ method
-        nlp_model = CTDirect.ExaBackend()
-    else
-        error("no known model (:adnlp, :exa) in method", method)
-    end
-
-    # patch: replaces ipopt by madnlp for :exa as long as the issue with getters for a posteriori treatment is not fixed
-    if (:exa ∈ method) && (:ipopt ∈ method)
-        nlp_solver = CTDirect.MadNLPBackend()
-        @warn "currently replacing Ipopt with MadNLP for :exa"
-    end
-
-    return nlp_solver, nlp_model
 end
 
 """
@@ -154,7 +160,8 @@ function solve(
     )
 
     # get NLP solver choice and solve DOCP
-    nlp_solver, nlp_model = parse_description(description)
+    nlp_solver = parse_description(description, :solver)
+    nlp_model = parse_description(description, :model)
     docp_solution = CTDirect.solve_docp(nlp_solver, docp; display=display, kwargs...)
 
     # build and return OCP solution
@@ -226,7 +233,9 @@ function direct_transcription(
     lagrange_to_mayer=__lagrange_to_mayer(),
     kwargs...,
 )
-    nlp_solver, nlp_model = parse_description(description)
+
+    #nlp_solver = parse_description(description, :solver)
+    nlp_model = parse_description(description, :model)
 
     # build DOCP
     if nlp_model isa ExaBackend
@@ -265,9 +274,9 @@ function direct_transcription(
     # build nlp
     build_nlp!(
         docp,
-        nlp_model,
+        nlp_model, # is now in docp, can be removed
         x0;
-        nlp_solver=nlp_solver,
+        #nlp_solver=nlp_solver,
         grid_size=grid_size,
         disc_method=disc_method,
         kwargs...,
