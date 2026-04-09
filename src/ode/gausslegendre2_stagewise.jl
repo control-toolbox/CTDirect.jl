8c15e13b51683e31d45957f5b13f3cc09648eb98
vmerc <valentin.0111@hotmail.fr>
2026-04-09 14:43:26 +0200

    commentaires français -> anglais

--- src/ode/gausslegendre2_stagewise.jl
+++ src/ode/gausslegendre2_stagewise.jl
@@ -3,14 +3,14 @@
 #
 # Minimal specialized stagewise version of Gauss-Legendre 2 collocation.
 #
-# Internal layout for NLP variables (vecteur xu):
+# Internal layout for NLP variables (vector xu):
 # [X_0, U_0^1, U_0^2, K_0^1, K_0^2,
 #  X_1, U_1^1, U_1^2, K_1^1, K_1^2,
 #  ...
 #  X_N-1, U_N-1^1, U_N-1^2, K_N-1^1, K_N-1^2,
 #  X_N, V]
 #
-# Internal layout for NLP constraints (vecteur c):
+# Internal layout for NLP constraints (vector c):
 # [C_0^x, C_0^{k,1}, C_0^{k,2}, G_0,
 #  C_1^x, C_1^{k,1}, C_1^{k,2}, G_1,
 #  ...
@@ -18,19 +18,19 @@
 #  G_N,
 #  B]
 #
-# avec :
-# C_i^x      = contrainte d'état sur le pas i
+# with:
+# C_i^x      = state constraint on step i
 #              X_{i+1} - (X_i + h_i * sum_j b_j K_i^j)
 #
-# C_i^{k,1}  = contrainte de stage 1 sur le pas i
+# C_i^{k,1}  = stage-1 constraint on step i
 #              K_i^1 - f(t_i^1, X_i^1, U_i^1, V)
 #
-# C_i^{k,2}  = contrainte de stage 2 sur le pas i
+# C_i^{k,2}  = stage-2 constraint on step i
 #              K_i^2 - f(t_i^2, X_i^2, U_i^2, V)
 #
-# G_i        = contraintes de chemin au temps t_i   (si path_cons > 0)
-# G_N        = contraintes de chemin au temps final (si path_cons > 0)
-# B          = contraintes de bord /terminale 
+# G_i        = path constraints at time t_i   (if path_cons > 0)
+# G_N        = path constraints at final time (if path_cons > 0)
+# B          = boundary / terminal constraints
 
 abstract type GenericIRKStagewise <: Scheme end
 
@@ -77,17 +77,17 @@ end
 
 """
 Dimensions for the specialized Gauss-Legendre-2 stagewise scheme.
-    rappel : 
-    dims.NLP_x : nombre de variables d’état
-    dims.NLP_u : nombre de variables de contrôle
-    time.control_steps : pas bien compris je suppose à 1 ici 
-    dims.NLP_v : nombre de variables d’opti supplémentaires  (temps final ou initial libre par ex)
+    reminder:
+    dims.NLP_x : number of state variables
+    dims.NLP_u : number of control variables
+    time.control_steps : not fully clear yet, assumed to be 1 here
+    dims.NLP_v : number of additional optimization variables (free final or initial time for example)
 
-    retour : 
-    step_variables_block : contient le nombre de variable à mémoriser pour un seul timestep 
-    state_stage_eqs_block : combien size de vecteur d'état on stocke pour les équation ? equ d'état et les deux stages 
-    dim_NLP_variables : le nombre total de variables d’optimisation du problème , tous les pas plus le pas final 
-    dim_NLP_constraints : pour chaque pas de temps*((les contraintes d’état + stage)+ les contraintes de chemin) + les contraintes de chemin au temps final + les contraintes de bord
+    returns:
+    step_variables_block : number of variables stored for a single time step
+    state_stage_eqs_block : how many state-vector-sized blocks are stored for the equations, i.e. the state equation and the two stages
+    dim_NLP_variables : total number of optimization variables in the problem, all steps plus the final step
+    dim_NLP_constraints : for each time step, ((state + stage constraints) + path constraints) + path constraints at final time + boundary constraints
 
 """
 function GL2_stagewise_dims(dims::DOCPdims, time::DOCPtime)
@@ -133,25 +133,25 @@ function get_stagecontrol_at_time_step(xu, docp::DOCP{<:Gauss_Legendre_2_Stagewi
     dims = docp.dims
 
     @assert 1 <= j <= 2
-    #decalage pour se mettre au départ du block 
-    #X_i, U_i^1, U_i^2, K_i^1, K_i^2,
+    # offset to move to the start of the block
+    # X_i, U_i^1, U_i^2, K_i^1, K_i^2,
     var_offset = (i - 1) * disc._step_variables_block
-    #on se retrouve la :  U_i^1, U_i^2, K_i^1, K_i^2
+    # we now are here: U_i^1, U_i^2, K_i^1, K_i^2
     x_block_end = var_offset + dims.NLP_x
-    #on déf départ et fin en fct de la taille du controle 
+    # define start and end as a function of the control size
     ctrl0 = x_block_end + (j - 1) * disc._control_block + 1
     ctrl1 = x_block_end + j * disc._control_block
-    #on retourne la view 
+    # return the view
     return @view xu[ctrl0:ctrl1]
 end
 
 """
 Compatibility accessor: return a "control at time step".
 By default we return U_i^1.
-On retourne le controle N à N+1 si j n'est pas précisé on 
-retourne le controle j=1 
+We return control N at N+1; if j is not specified,
+we return control j=1.
 """
-#gestion du dernier pas de temps 
+# handle the last time step
 function get_OCP_control_at_time_step(
     xu,
     docp::DOCP{<:Gauss_Legendre_2_Stagewise},
@@ -164,7 +164,7 @@ function get_OCP_control_at_time_step(
 
     return get_stagecontrol_at_time_step(xu, docp, i, j)
 end
-#multiple dispatch si j n'est pas précisé 
+# multiple dispatch if j is not specified
 function get_OCP_control_at_time_step(xu, docp::DOCP{<:Gauss_Legendre_2_Stagewise}, i::Int)
     return get_OCP_control_at_time_step(xu, docp, i, 1)
 end
@@ -172,7 +172,7 @@ end
 """
 Return stage variables K_i^j on time step i.
 Convention: 1 <= i <= time.steps, j in {1,2}.
-On extrait K_i^j
+We extract K_i^j.
 """
 function get_stagevars_at_time_step(xu, docp::DOCP{<:Gauss_Legendre_2_Stagewise}, i::Int, j::Int)
     disc = disc_model(docp)
@@ -207,13 +207,13 @@ end
 # Bounds + initial guess
 # ----------------------------------------------------------------------------
 """
-Donner les bornes de chacune des variables du NLP 
+Give the bounds for each NLP variable.
 var_l <= xu <= var_u
-var_l et var_u ont la meme structure que xu donc on peut 
-réutiliser les meme fonction pour accéder au bon endroit
+var_l and var_u have the same structure as xu, so we can
+reuse the same functions to access the correct location.
 """
 function __variables_bounds!(docp::DOCP{<:Gauss_Legendre_2_Stagewise})
-    #on récupère les bornes et le modele continu 
+    # retrieve the bounds and the continuous model
     var_l = docp.bounds.var_l
     var_u = docp.bounds.var_u
     ocp = ocp_model(docp)
@@ -234,14 +234,14 @@ function __variables_bounds!(docp::DOCP{<:Gauss_Legendre_2_Stagewise})
         set_state_at_time_step!(var_l, x_lb, docp, i)
         set_state_at_time_step!(var_u, x_ub, docp, i)
     end
-    #si il y a une variable de controle (modif nouvelle version controle 0)
+    # if there is a control variable (change in the new zero-control version)
     if docp.dims.NLP_u > 0
         #for each timestep 
         for i in 1:docp.time.steps
             #for each control stage 
             for j in 1:2
-                # on accède au bon indice et on utilise la view pour Set 
-                # à modifier avec un setter propre 
+                # access the correct index and use the view to set it
+                # should later be replaced with a proper setter
                 get_stagecontrol_at_time_step(var_l, docp, i, j) .= u_lb
                 get_stagecontrol_at_time_step(var_u, docp, i, j) .= u_ub
             end
@@ -254,8 +254,8 @@ function __variables_bounds!(docp::DOCP{<:Gauss_Legendre_2_Stagewise})
             CTModels.dim_variable_constraints_box(ocp),
             CTModels.variable_constraints_box(ocp),
         )
-        #stocked at the end of xu so at the end of var_l and var_u 
-        #too so old setter are ok 
+        # stored at the end of xu, therefore also at the end of var_l and var_u
+        # so the old setters still work
         set_optim_variable!(var_l, v_lb, docp)
         set_optim_variable!(var_u, v_ub, docp)
     end
@@ -267,18 +267,18 @@ function __initial_guess(
     docp::DOCP{<:Gauss_Legendre_2_Stagewise},
     init::CTModels.InitialGuess,
 )   
-    #everything to 0.1 
+    # initialize everything to 0.1
     NLP_X = 0.1 * ones(docp.dim_NLP_variables)
     disc = disc_model(docp)
-    #si on a des init variable on mets les valeurs init dispo à la bonne place 
+    # if we have initial variable values, place the available values in the correct location
     set_optim_variable!(NLP_X, init.variable, docp)
-    #on remplie les ti avec init state 
+    # fill the t_i points with the initial state
     time_grid = get_time_grid(NLP_X, docp)
     for i in 1:(docp.time.steps + 1)
         ti = time_grid[i]
         set_state_at_time_step!(NLP_X, init.state(ti), docp, i)
     end
-    # si controle 
+    # if there is a control
     if docp.dims.NLP_u > 0
         for i in 1:docp.time.steps
             ti = time_grid[i]
@@ -311,36 +311,36 @@ function integral(docp::DOCP{<:Gauss_Legendre_2_Stagewise}, xu, v, time_grid, f)
 
     value = 0.0
     work_xij = similar(xu, dims.NLP_x)
-    #pour chaque pas de temps i 
+    # for each time step i
     for i in 1:docp.time.steps
-        #temps de départ ti 
+        # starting time ti
         ti = time_grid[i]
-        #prendre l’état au début xi
+        # get the state at the beginning xi
         xi = get_OCP_state_at_time_step(xu, docp, i)
-        #calculer la taille du pas hi
+        # compute the step size hi
         hi = time_grid[i + 1] - ti
 
         local_sum = 0.0
-        #pour chaque stage 
+        # for each stage
         for j in 1:2
-            #calculer le temps du stage
+            # compute the stage time
             tij = ti + disc.butcher_c[j] * hi
-            #récupérer le contrôle de stage uij
+            # retrieve the stage control uij
             uij = get_stagecontrol_at_time_step(xu, docp, i, j)
-            #reconstruire l’état au stage xij
-            #on copie x_i dans work_xij
+            # reconstruct the stage state xij
+            # copy x_i into work_xij
             @views @. work_xij = xi
-            #pour chaque stage (implicite)
+            # for each stage (implicit)
             for l in 1:2
-                #Récupération de K_i^l
+                # retrieve K_i^l
                 kil = get_stagevars_at_time_step(xu, docp, i, l)
-                #Mise à jour de work_xij
+                # update work_xij
                 @views @. work_xij = work_xij + hi * disc.butcher_a[j, l] * kil
             end
-            #evaluer le cout en f(tij,xij,uij,v) et l'ajouter pondéré par bj 
+            # evaluate the cost f(tij,xij,uij,v) and add it weighted by b_j
             local_sum += disc.butcher_b[j] * f(tij, work_xij, uij, v)
         end
-        #ajouter hi * somme_locale à la valeur totale
+        # add hi * local_sum to the total value
         value += hi * local_sum
     end
 
@@ -369,39 +369,39 @@ function stepStateConstraints!(
     dims = docp.dims
 
     # work layout: [x_ij ; sum_bk]
-    #allocate for work defined in setWorkArray
+    # allocate from the work vector defined in setWorkArray
     work_xij = @view work[1:dims.NLP_x]
     work_sumbk = @view work[(dims.NLP_x + 1):(2 * dims.NLP_x)]
-    #calculer l'endroit ou commencent les contraintes du pas i 
+    # compute where the constraints for step i start
     offset = (i - 1) * (disc._state_stage_eqs_block + disc._step_pathcons_block)
-    #début fin et taille du pas 
+    # start, end, and size of the step
     ti = time_grid[i]
     tip1 = time_grid[i + 1]
     hi = tip1 - ti
 
-    #etat aux extremités du pas 
+    # state at the ends of the step
     xi = get_OCP_state_at_time_step(xu, docp, i)
     xip1 = get_OCP_state_at_time_step(xu, docp, i + 1)
-    #première dim réservées à équation d'état 
+    # first dimensions reserved for the state equation
     offset_stage_eqs = dims.NLP_x
-    #pour les stages 
+    # for the stages
     for j in 1:2
         tij = ti + disc.butcher_c[j] * hi
         kij = get_stagevars_at_time_step(xu, docp, i, j)
         uij = get_stagecontrol_at_time_step(xu, docp, i, j)
-        #construire Σ b_j K_i^j
+        # build Σ b_j K_i^j
         if j == 1
             @views @. work_sumbk = disc.butcher_b[j] * kij
         else
             @views @. work_sumbk = work_sumbk + disc.butcher_b[j] * kij
         end
-        #x_i^j = x_i + h_i Σ_l a_{j,l} K_i^l
+        # x_i^j = x_i + h_i Σ_l a_{j,l} K_i^l
         @views @. work_xij = xi
         for l in 1:2
             kil = get_stagevars_at_time_step(xu, docp, i, l)
             @views @. work_xij = work_xij + hi * disc.butcher_a[j, l] * kil
         end
-        #on calcule f(t_i^j, x_i^j, u_i^j, v)
+        # compute f(t_i^j, x_i^j, u_i^j, v)
         CTModels.dynamics(ocp)(
             @view(c[(offset + offset_stage_eqs + 1):(offset + offset_stage_eqs + dims.NLP_x)]),
             tij,
@@ -409,14 +409,14 @@ function stepStateConstraints!(
             uij,
             v,
         )
-        #on stocke K_i^j - f(t_i^j, x_i^j, u_i^j, v)
-        # rappel @views @. -> sans réallouer élément par élément 
+        # store K_i^j - f(t_i^j, x_i^j, u_i^j, v)
+        # reminder: @views @. -> without reallocating, element by element
         @views @. c[(offset + offset_stage_eqs + 1):(offset + offset_stage_eqs + dims.NLP_x)] =
             kij - c[(offset + offset_stage_eqs + 1):(offset + offset_stage_eqs + dims.NLP_x)]
-        #offset contrainte suivante 
+        # offset for the next constraint
         offset_stage_eqs += dims.NLP_x
     end
-    #contrainte d'évolution de l'état x_{i+1} - (x_i + h_i Σ_j b_j K_i^j)
+    # state evolution constraint x_{i+1} - (x_i + h_i Σ_j b_j K_i^j)
     @views @. c[(offset + 1):(offset + dims.NLP_x)] =
         xip1 - (xi + hi * work_sumbk)
 
